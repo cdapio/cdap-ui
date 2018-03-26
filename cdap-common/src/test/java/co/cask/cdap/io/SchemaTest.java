cdapio/cdap

(CDAP-13143) Supports recursive Schema correctly

cdap-common/src/test/java/co/cask/cdap/io/SchemaTest.java
@@ -373,6 +373,98 @@ public class SchemaTest {
     }
   }
 
+  /**
+   * Parent class for testing recursive schema.
+   */
+  private static final class NodeParent {
+    private List<NodeChild> children;
+  }
+
+  /**
+   * Child class for testing recursive schema.
+   */
+  private static final class NodeChild {
+    private int data;
+    private NodeParent parent;
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testGeneratorRecursion() throws Exception {
+    // This test the schema generator be able to handle recursive schema between multiple classes
+    Schema generatedSchema = new ReflectionSchemaGenerator(false).generate(NodeParent.class);
+
+    // Validate the NodeChild.parent schema is the same as the schema of the Parent class
+    // The schema generator always generate array value as nullable union
+    Schema childSchema = generatedSchema.getField("children").getSchema().getComponentSchema().getNonNullable();
+    Assert.assertEquals(generatedSchema, childSchema.getField("parent").getSchema());
+
+    // Serialize the schema and then parse it back
+    Schema deserializedSchema = Schema.parseJson(generatedSchema.toString());
+    Assert.assertEquals(generatedSchema, deserializedSchema);
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testRecursion() throws Exception {
+    // Test simple recursion
+    Schema nodeSchema = Schema.recordOf("Node",
+                                        Schema.Field.of("data", Schema.of(Schema.Type.INT)),
+                                        Schema.Field.of("next", Schema.recordOf("Node")));
+    Assert.assertEquals(nodeSchema, nodeSchema.getField("next").getSchema());
+
+    // Serialize and deserialize and validate
+    Assert.assertEquals(nodeSchema, Schema.parseJson(nodeSchema.toString()));
+    Assert.assertEquals(nodeSchema, Schema.parseJson(nodeSchema.toString()).getField("next").getSchema());
+
+    // Test multi-levels recursion. It has the following structure:
+    // Parent {
+    //   List<Child> children
+    // }
+    // Child {
+    //   GrantChild child
+    // }
+    // GrantChild {
+    //   Child parent
+    //   Parent grantParent
+    // }
+
+    Schema parentSchema = Schema.recordOf(
+      "Parent", Schema.Field.of("children", Schema.arrayOf(Schema.recordOf(
+        "Child", Schema.Field.of("children", Schema.mapOf(Schema.of(Schema.Type.STRING), Schema.recordOf(
+          "GrantChild", Schema.Field.of("parent", Schema.recordOf("Child")),
+          Schema.Field.of("grantParent", Schema.recordOf("Parent"))
+        )))))));
+
+    Schema childSchema = parentSchema.getField("children").getSchema().getComponentSchema();
+    Schema grantChildSchema = childSchema.getField("children").getSchema().getMapSchema().getValue();
+    Assert.assertEquals(parentSchema, grantChildSchema.getField("grantParent").getSchema());
+    Assert.assertEquals(childSchema, grantChildSchema.getField("parent").getSchema());
+
+    // Serialize and deserialize it back and validate again
+    parentSchema = Schema.parseJson(parentSchema.toString());
+    Assert.assertEquals(parentSchema, grantChildSchema.getField("grantParent").getSchema());
+    Assert.assertEquals(childSchema, grantChildSchema.getField("parent").getSchema());
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testUnionRecordReference() throws Exception {
+    // Test records defined earlier in an union can be referred by name in schema defined later in the union.
+    Schema first = Schema.recordOf("First", Schema.Field.of("data", Schema.of(Schema.Type.INT)));
+    Schema second = Schema.recordOf("Second", Schema.Field.of("firsts", Schema.arrayOf(Schema.recordOf("First"))));
+
+    Schema union = Schema.unionOf(first, second);
+
+    // Validate the schema are resolved
+    Assert.assertEquals(first, union.getUnionSchemas().get(1).getField("firsts").getSchema().getComponentSchema());
+
+    // Serialize and deserialize it back and validate again
+    union = Schema.parseJson(union.toString());
+    Assert.assertEquals(first, union.getUnionSchemas().get(1).getField("firsts").getSchema().getComponentSchema());
+  }
+
+
   private void verifyThrowsException(String toParse) {
     try {
       Schema.parseSQL(toParse);

cdapio/cdap-ui

cdap-common/src/test/java/co/cask/cdap/io/SchemaTest.java
@@ -373,6 +373,98 @@ public class SchemaTest {
     }
   }
 
+  /**
+   * Parent class for testing recursive schema.
+   */
+  private static final class NodeParent {
+    private List<NodeChild> children;
+  }
+
+  /**
+   * Child class for testing recursive schema.
+   */
+  private static final class NodeChild {
+    private int data;
+    private NodeParent parent;
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testGeneratorRecursion() throws Exception {
+    // This test the schema generator be able to handle recursive schema between multiple classes
+    Schema generatedSchema = new ReflectionSchemaGenerator(false).generate(NodeParent.class);
+
+    // Validate the NodeChild.parent schema is the same as the schema of the Parent class
+    // The schema generator always generate array value as nullable union
+    Schema childSchema = generatedSchema.getField("children").getSchema().getComponentSchema().getNonNullable();
+    Assert.assertEquals(generatedSchema, childSchema.getField("parent").getSchema());
+
+    // Serialize the schema and then parse it back
+    Schema deserializedSchema = Schema.parseJson(generatedSchema.toString());
+    Assert.assertEquals(generatedSchema, deserializedSchema);
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testRecursion() throws Exception {
+    // Test simple recursion
+    Schema nodeSchema = Schema.recordOf("Node",
+                                        Schema.Field.of("data", Schema.of(Schema.Type.INT)),
+                                        Schema.Field.of("next", Schema.recordOf("Node")));
+    Assert.assertEquals(nodeSchema, nodeSchema.getField("next").getSchema());
+
+    // Serialize and deserialize and validate
+    Assert.assertEquals(nodeSchema, Schema.parseJson(nodeSchema.toString()));
+    Assert.assertEquals(nodeSchema, Schema.parseJson(nodeSchema.toString()).getField("next").getSchema());
+
+    // Test multi-levels recursion. It has the following structure:
+    // Parent {
+    //   List<Child> children
+    // }
+    // Child {
+    //   GrandChild child
+    // }
+    // GrandChild {
+    //   Child parent
+    //   Parent grandParent
+    // }
+
+    Schema parentSchema = Schema.recordOf(
+      "Parent", Schema.Field.of("children", Schema.arrayOf(Schema.recordOf(
+        "Child", Schema.Field.of("children", Schema.mapOf(Schema.of(Schema.Type.STRING), Schema.recordOf(
+          "GrandChild", Schema.Field.of("parent", Schema.recordOf("Child")),
+          Schema.Field.of("grandParent", Schema.recordOf("Parent"))
+        )))))));
+
+    Schema childSchema = parentSchema.getField("children").getSchema().getComponentSchema();
+    Schema grandChildSchema = childSchema.getField("children").getSchema().getMapSchema().getValue();
+    Assert.assertEquals(parentSchema, grandChildSchema.getField("grandParent").getSchema());
+    Assert.assertEquals(childSchema, grandChildSchema.getField("parent").getSchema());
+
+    // Serialize and deserialize it back and validate again
+    parentSchema = Schema.parseJson(parentSchema.toString());
+    Assert.assertEquals(parentSchema, grandChildSchema.getField("grandParent").getSchema());
+    Assert.assertEquals(childSchema, grandChildSchema.getField("parent").getSchema());
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testUnionRecordReference() throws Exception {
+    // Test records defined earlier in an union can be referred by name in schema defined later in the union.
+    Schema first = Schema.recordOf("First", Schema.Field.of("data", Schema.of(Schema.Type.INT)));
+    Schema second = Schema.recordOf("Second", Schema.Field.of("firsts", Schema.arrayOf(Schema.recordOf("First"))));
+
+    Schema union = Schema.unionOf(first, second);
+
+    // Validate the schema are resolved
+    Assert.assertEquals(first, union.getUnionSchemas().get(1).getField("firsts").getSchema().getComponentSchema());
+
+    // Serialize and deserialize it back and validate again
+    union = Schema.parseJson(union.toString());
+    Assert.assertEquals(first, union.getUnionSchemas().get(1).getField("firsts").getSchema().getComponentSchema());
+  }
+
+
   private void verifyThrowsException(String toParse) {
     try {
       Schema.parseSQL(toParse);