shiyuren/cdap

CDAP-13260 Support CLI for metadata entity and tests

cdap-cli-tests/src/test/java/co/cask/cdap/cli/CLIMainTest.java
@@ -18,7 +18,9 @@ package co.cask.cdap.cli;
 
 import co.cask.cdap.StandaloneTester;
 import co.cask.cdap.api.artifact.ArtifactScope;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.command.NamespaceCommandUtils;
+import co.cask.cdap.cli.command.metadata.MetadataCommandHelper;
 import co.cask.cdap.cli.util.RowMaker;
 import co.cask.cdap.cli.util.table.Table;
 import co.cask.cdap.client.DatasetTypeClient;
@@ -849,6 +851,7 @@ public class CLIMainTest extends CLITestBase {
     testCommandOutputContains(cli, String.format("get metadata-properties %s", FAKE_DS_ID), "dsKey,dsValue");
     testCommandOutputContains(cli, String.format("add metadata-tags %s 'streamTag1 streamTag2'", FAKE_STREAM_ID),
                               "Successfully added metadata tags");
+
     output = getCommandOutput(cli, String.format("get metadata-tags %s", FAKE_STREAM_ID));
     lines = Arrays.asList(output.split("\\r?\\n"));
     Assert.assertTrue(lines.contains("streamTag1") && lines.contains("streamTag2"));
@@ -895,6 +898,54 @@ public class CLIMainTest extends CLITestBase {
     lines = Arrays.asList(output.split("\\r?\\n"));
     expected = ImmutableList.of("Entity", FAKE_WORKFLOW_ID.toString());
     Assert.assertTrue(lines.containsAll(expected) && expected.containsAll(lines));
+
+    MetadataEntity fieldEntity =
+      MetadataEntity.builder(FAKE_DS_ID.toMetadataEntity()).appendAsType("field", "empName").build();
+
+    testCommandOutputContains(cli, String.format("add metadata-tags %s 'fieldTag1 fieldTag2 fieldTag3'",
+                                                 MetadataCommandHelper.toString(fieldEntity)),
+                              "Successfully added metadata tags");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s", MetadataCommandHelper.toString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    Assert.assertTrue(lines.contains("fieldTag1") && lines.contains("fieldTag2") && lines.contains("fieldTag3"));
+    testCommandOutputContains(cli, String.format("add metadata-properties %s fieldKey=fieldValue",
+                                                 MetadataCommandHelper.toString(fieldEntity)),
+                              "Successfully added metadata properties");
+    testCommandOutputContains(cli, String.format("get metadata-properties %s",
+                                                 MetadataCommandHelper.toString(fieldEntity)), "fieldKey,fieldValue");
+
+    // test that retrieving metadata for EntityId is returned in old format
+    testCommandOutputContains(cli, String.format("get metadata %s", FAKE_STREAM_ID), FAKE_STREAM_ID.toString());
+    // test get metadata for custom entity and verify that return is in new format
+    testCommandOutputContains(cli, String.format("get metadata %s", MetadataCommandHelper.toString(fieldEntity)),
+                              fieldEntity.toString());
+
+    // test remove
+    testCommandOutputContains(cli, String.format("remove metadata-tag %s 'fieldTag3'",
+                                                 MetadataCommandHelper.toString(fieldEntity)),
+                              "Successfully removed metadata tag");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s", MetadataCommandHelper.toString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain the removed tag
+    Assert.assertTrue(lines.contains("fieldTag1") && lines.contains("fieldTag2") && !lines.contains("fieldTag3"));
+    testCommandOutputContains(cli, String.format("remove metadata-tags %s",
+                                                 MetadataCommandHelper.toString(fieldEntity)),
+                              "Successfully removed metadata tags");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s", MetadataCommandHelper.toString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain any tags except the header added by cli
+    Assert.assertTrue(lines.size() == 1 && lines.contains("tags"));
+
+    testCommandOutputContains(cli, String.format("remove metadata-properties %s",
+                                                 MetadataCommandHelper.toString(fieldEntity)),
+                              "Successfully removed metadata properties");
+
+    // test remove properties
+    output = getCommandOutput(cli, String.format("get metadata-properties %s",
+                                                 MetadataCommandHelper.toString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain any properties except the header added by cli
+    Assert.assertTrue(lines.size() == 1 && lines.contains("key,value"));
   }
 
   private static File createAppJarFile(Class<?> cls) throws IOException {

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/GetMetadataPropertiesCommand.java
@@ -16,13 +16,13 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.api.metadata.MetadataScope;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.cli.util.table.Table;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
@@ -49,10 +49,11 @@ public class GetMetadataPropertiesCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.getMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String scope = arguments.getOptional(ArgumentName.METADATA_SCOPE.toString());
-    Map<String, String> properties = scope == null ? client.getProperties(entity) :
-      client.getProperties(entity, MetadataScope.valueOf(scope.toUpperCase()));
+    Map<String, String> properties = scope == null ? client.getProperties(metadataEntity) :
+      client.getProperties(metadataEntity, MetadataScope.valueOf(scope.toUpperCase()));
 
     Table table = Table.builder()
       .setHeader("key", "value")

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataPropertiesCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,8 +41,9 @@ public class RemoveMetadataPropertiesCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
-    client.removeProperties(entity);
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.getMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
+    client.removeProperties(metadataEntity);
     output.println("Successfully removed metadata properties");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataPropertyCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,9 +41,10 @@ public class RemoveMetadataPropertyCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.getMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String property = arguments.get("property");
-    client.removeProperty(entity, property);
+    client.removeProperty(metadataEntity, property);
     output.println("Successfully removed metadata property");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataTagCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,9 +41,10 @@ public class RemoveMetadataTagCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.getMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String tag = arguments.get("tag");
-    client.removeTag(entity, tag);
+    client.removeTag(metadataEntity, tag);
     output.println("Successfully removed metadata tag");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataTagsCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,8 +41,9 @@ public class RemoveMetadataTagsCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
-    client.removeTags(entity);
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.getMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
+    client.removeTags(metadataEntity);
     output.println("Successfully removed metadata tags");
   }
 

cdapio/cdap-ui

cdap-cli-tests/src/test/java/co/cask/cdap/cli/CLIMainTest.java
@@ -18,7 +18,9 @@ package co.cask.cdap.cli;
 
 import co.cask.cdap.StandaloneTester;
 import co.cask.cdap.api.artifact.ArtifactScope;
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.command.NamespaceCommandUtils;
+import co.cask.cdap.cli.command.metadata.MetadataCommandHelper;
 import co.cask.cdap.cli.util.RowMaker;
 import co.cask.cdap.cli.util.table.Table;
 import co.cask.cdap.client.DatasetTypeClient;
@@ -848,6 +850,7 @@ public class CLIMainTest extends CLITestBase {
     testCommandOutputContains(cli, String.format("get metadata-properties %s", FAKE_DS_ID), "dsKey,dsValue");
     testCommandOutputContains(cli, String.format("add metadata-tags %s 'streamTag1 streamTag2'", FAKE_STREAM_ID),
                               "Successfully added metadata tags");
+
     output = getCommandOutput(cli, String.format("get metadata-tags %s", FAKE_STREAM_ID));
     lines = Arrays.asList(output.split("\\r?\\n"));
     Assert.assertTrue(lines.contains("streamTag1") && lines.contains("streamTag2"));
@@ -894,6 +897,58 @@ public class CLIMainTest extends CLITestBase {
     lines = Arrays.asList(output.split("\\r?\\n"));
     expected = ImmutableList.of("Entity", FAKE_WORKFLOW_ID.toString());
     Assert.assertTrue(lines.containsAll(expected) && expected.containsAll(lines));
+
+    MetadataEntity fieldEntity =
+      MetadataEntity.builder(FAKE_DS_ID.toMetadataEntity()).appendAsType("field", "empName").build();
+
+    testCommandOutputContains(cli, String.format("add metadata-tags %s 'fieldTag1 fieldTag2 fieldTag3'",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "Successfully added metadata tags");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    Assert.assertTrue(lines.contains("fieldTag1") && lines.contains("fieldTag2") && lines.contains("fieldTag3"));
+    testCommandOutputContains(cli, String.format("add metadata-properties %s fieldKey=fieldValue",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "Successfully added metadata properties");
+    testCommandOutputContains(cli, String.format("get metadata-properties %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "fieldKey,fieldValue");
+
+    // test that retrieving metadata for EntityId is returned in old format
+    testCommandOutputContains(cli, String.format("get metadata %s", FAKE_STREAM_ID), FAKE_STREAM_ID.toString());
+    // test get metadata for custom entity and verify that return is in new format
+    testCommandOutputContains(cli, String.format("get metadata %s", MetadataCommandHelper.toCliString(fieldEntity)),
+                              fieldEntity.toString());
+
+    // test remove
+    testCommandOutputContains(cli, String.format("remove metadata-tag %s 'fieldTag3'",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "Successfully removed metadata tag");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain the removed tag
+    Assert.assertTrue(lines.contains("fieldTag1") && lines.contains("fieldTag2") && !lines.contains("fieldTag3"));
+    testCommandOutputContains(cli, String.format("remove metadata-tags %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "Successfully removed metadata tags");
+    output = getCommandOutput(cli, String.format("get metadata-tags %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain any tags except the header added by cli
+    Assert.assertTrue(lines.size() == 1 && lines.contains("tags"));
+
+    testCommandOutputContains(cli, String.format("remove metadata-properties %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)),
+                              "Successfully removed metadata properties");
+
+    // test remove properties
+    output = getCommandOutput(cli, String.format("get metadata-properties %s",
+                                                 MetadataCommandHelper.toCliString(fieldEntity)));
+    lines = Arrays.asList(output.split("\\r?\\n"));
+    // should not contain any properties except the header added by cli
+    Assert.assertTrue(lines.size() == 1 && lines.contains("key,value"));
   }
 
   private static File createAppJarFile(Class<?> cls) throws IOException {

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/GetMetadataPropertiesCommand.java
@@ -16,13 +16,13 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.api.metadata.MetadataScope;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.cli.util.table.Table;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
@@ -49,10 +49,11 @@ public class GetMetadataPropertiesCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.toMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String scope = arguments.getOptional(ArgumentName.METADATA_SCOPE.toString());
-    Map<String, String> properties = scope == null ? client.getProperties(entity) :
-      client.getProperties(entity, MetadataScope.valueOf(scope.toUpperCase()));
+    Map<String, String> properties = scope == null ? client.getProperties(metadataEntity) :
+      client.getProperties(metadataEntity, MetadataScope.valueOf(scope.toUpperCase()));
 
     Table table = Table.builder()
       .setHeader("key", "value")

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataPropertiesCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,8 +41,9 @@ public class RemoveMetadataPropertiesCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
-    client.removeProperties(entity);
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.toMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
+    client.removeProperties(metadataEntity);
     output.println("Successfully removed metadata properties");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataPropertyCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,9 +41,10 @@ public class RemoveMetadataPropertyCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.toMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String property = arguments.get("property");
-    client.removeProperty(entity, property);
+    client.removeProperty(metadataEntity, property);
     output.println("Successfully removed metadata property");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataTagCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,9 +41,10 @@ public class RemoveMetadataTagCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.toMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
     String tag = arguments.get("tag");
-    client.removeTag(entity, tag);
+    client.removeTag(metadataEntity, tag);
     output.println("Successfully removed metadata tag");
   }
 

cdap-cli/src/main/java/co/cask/cdap/cli/command/metadata/RemoveMetadataTagsCommand.java
@@ -16,11 +16,11 @@
 
 package co.cask.cdap.cli.command.metadata;
 
+import co.cask.cdap.api.metadata.MetadataEntity;
 import co.cask.cdap.cli.ArgumentName;
 import co.cask.cdap.cli.CLIConfig;
 import co.cask.cdap.cli.util.AbstractCommand;
 import co.cask.cdap.client.MetadataClient;
-import co.cask.cdap.proto.id.EntityId;
 import co.cask.common.cli.Arguments;
 import com.google.inject.Inject;
 
@@ -41,8 +41,9 @@ public class RemoveMetadataTagsCommand extends AbstractCommand {
 
   @Override
   public void perform(Arguments arguments, PrintStream output) throws Exception {
-    EntityId entity = EntityId.fromString(arguments.get(ArgumentName.ENTITY.toString()));
-    client.removeTags(entity);
+    MetadataEntity metadataEntity =
+      MetadataCommandHelper.toMetadataEntity(arguments.get(ArgumentName.ENTITY.toString()));
+    client.removeTags(metadataEntity);
     output.println("Successfully removed metadata tags");
   }
 