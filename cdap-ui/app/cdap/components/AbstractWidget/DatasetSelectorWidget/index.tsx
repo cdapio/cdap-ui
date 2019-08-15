zuxqoj/cdap

[CDAP-15716] integrate new react widgets to abstract widget

cdap-ui/app/cdap/components/AbstractWidget/DatasetSelectorWidget/index.tsx
@@ -24,9 +24,10 @@ import classnames from 'classnames';
 import ee from 'event-emitter';
 import { objectQuery } from 'services/helpers';
 import ThemeWrapper from 'components/ThemeWrapper';
-import PropTypes from 'prop-types';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { KEY_CODE } from 'services/global-constants';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
+import { IWidgetProps } from 'components/AbstractWidget';
 
 const styles = (theme): StyleRules => {
   return {
@@ -78,26 +79,30 @@ const styles = (theme): StyleRules => {
   };
 };
 
-interface IDatasetSelector extends WithStyles<typeof styles> {
-  onChange: (value) => void;
-  value: string;
+interface IDatasetSelectorWidgetProps {
   placeholder?: string;
 }
+
+interface IDatasetSelectorProps
+  extends IWidgetProps<IDatasetSelectorWidgetProps>,
+    WithStyles<typeof styles> {}
 
 const RESULT_LIMIT = 5;
 
 export const DatasetSelectedEvent = 'dataset.selected';
 
-const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
+const DatasetSelectorView: React.SFC<IDatasetSelectorProps> = ({
   value,
   onChange,
   classes,
-  placeholder,
+  widgetProps,
+  disabled,
 }) => {
   const [datasets, setDatasets] = React.useState([]);
   const [showAutocomplete, setShowAutoComplete] = React.useState(false);
   const [selection, setSelection] = React.useState(0);
   const eventEmitter = ee(ee);
+  const placeholder = objectQuery(widgetProps, 'placeholder');
 
   React.useEffect(() => {
     MyDatasetApi.list({ namespace: getCurrentNamespace() }).subscribe(setDatasets);
@@ -109,13 +114,13 @@ const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
   }
 
   function getMatch() {
-    if (!value || value.length === 0) {
+    if (!value || value.toString().length === 0) {
       return datasets.slice(0, RESULT_LIMIT);
     }
     return datasets
       .filter((dataset) => {
         const datasetName = dataset.name.toLowerCase();
-        const inputValue = value.toLowerCase();
+        const inputValue = value.toString().toLowerCase();
         return datasetName.indexOf(inputValue) !== -1;
       })
       .slice(0, RESULT_LIMIT);
@@ -192,6 +197,7 @@ const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
           onKeyDown={handleKeyDown}
           onKeyPress={handleKeyPress}
           placeholder={placeholder}
+          disabled={disabled}
           onFocus={() => setShowAutoComplete(true)}
           InputProps={{
             classes: {
@@ -237,10 +243,6 @@ function DatasetSelector(props) {
     </ThemeWrapper>
   );
 }
-(DatasetSelector as any).propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-};
+(DatasetSelector as any).propTypes = WIDGET_PROPS;
 
 export default DatasetSelector;

cdap-ui/app/cdap/components/AbstractWidget/FormInputs/TextBox/index.tsx
@@ -16,9 +16,11 @@
 
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import PropTypes from 'prop-types';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import ThemeWrapper from 'components/ThemeWrapper';
+import { objectQuery } from 'services/helpers';
+import { IWidgetProps } from 'components/AbstractWidget';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
 
 const styles = (theme) => {
   return {
@@ -40,19 +42,23 @@ const styles = (theme) => {
     notchedOutline: {},
   };
 };
-interface ITextBoxProps extends WithStyles<typeof styles> {
-  value: string;
-  onChange: (value: string) => void;
-  placeholder: string;
+
+interface ITextBoxWidgetProps {
+  placeholder?: string;
 }
 
-function TextBox({ value, onChange, placeholder, classes }: ITextBoxProps) {
+interface ITextBoxProps extends IWidgetProps<ITextBoxWidgetProps>, WithStyles<typeof styles> {}
+
+const TextBox: React.FC<ITextBoxProps> = ({ value, onChange, widgetProps, disabled, classes }) => {
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const v = event.target.value;
     if (typeof onChange === 'function') {
       onChange(v);
     }
   };
+
+  const placeholder = objectQuery(widgetProps, 'placeholder');
+
   return (
     <TextField
       fullWidth
@@ -61,12 +67,13 @@ function TextBox({ value, onChange, placeholder, classes }: ITextBoxProps) {
       onChange={onChangeHandler}
       className={classes.root}
       placeholder={placeholder}
+      disabled={disabled}
       InputProps={{
         classes,
       }}
     />
   );
-}
+};
 const StyledTextBox = withStyles(styles)(TextBox);
 export default function TextBoxWrapper(props) {
   return (
@@ -75,8 +82,5 @@ export default function TextBoxWrapper(props) {
     </ThemeWrapper>
   );
 }
-(TextBoxWrapper as any).propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-};
+
+(TextBoxWrapper as any).propTypes = WIDGET_PROPS;

cdap-ui/app/cdap/components/AbstractWidget/JoinTypeWidget/index.tsx
@@ -15,17 +15,17 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import withStyles, { WithStyles, StyleRules } from '@material-ui/core/styles/withStyles';
 import isEqual from 'lodash/isEqual';
-
-import Select from 'components/AbstractWidget/SelectWidget';
+import Select from 'components/AbstractWidget/FormInputs/Select';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Paper from '@material-ui/core/Paper';
-
 import ThemeWrapper from 'components/ThemeWrapper';
 import If from 'components/If';
+import { IWidgetProps } from 'components/AbstractWidget';
+import { objectQuery } from 'services/helpers';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
 
 const styles = (theme): StyleRules => {
   return {
@@ -69,23 +69,21 @@ const styles = (theme): StyleRules => {
   };
 };
 
-interface IJoinTypeWidgetProps extends WithStyles<typeof styles> {
-  value: string;
-  inputSchema: Array<{ name: string }>;
-  onChange: (value: string) => void;
-}
+interface IJoinTypeWidgetProps extends IWidgetProps<null>, WithStyles<typeof styles> {}
 
 const DROP_DOWN_OPTIONS: string[] = ['Inner', 'Outer'];
 
 const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
   value,
-  inputSchema,
+  extraConfig,
   onChange,
+  disabled,
   classes,
 }) => {
   const [joinType, setJoinType] = useState(DROP_DOWN_OPTIONS[1]);
   const [selectedCount, setSelectedCount] = useState(0);
   const [inputs, setInputs] = useState([]);
+  const inputSchema = objectQuery(extraConfig, 'inputSchema');
 
   const formatOutput = () => {
     const outputArr = inputs.filter((schema) => schema.selected).map((schema) => schema.name);
@@ -93,11 +91,11 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
     setSelectedCount(outputArr.length);
   };
 
-  const joinTypeChange = (event) => {
-    setJoinType(event.target.value);
+  const joinTypeChange = (val) => {
+    setJoinType(val);
     setInputs(
       inputSchema.map((input) => {
-        return { name: input.name, selected: event.target.value === 'Inner' };
+        return { name: input.name, selected: val === 'Inner' };
       })
     );
   };
@@ -122,7 +120,10 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
       );
       return;
     }
-    const initialModel = value.split(',').map((input) => input.trim());
+    const initialModel = value
+      .toString()
+      .split(',')
+      .map((input) => input.trim());
     if (isEqual(initialModel, inputSchema.map((schema) => schema.name.trim()))) {
       setJoinType('Inner');
       setInputs(
@@ -155,9 +156,10 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
       <If condition={inputs.length > 0}>
         <Paper className={classes.multiCheckboxesContainer}>
           <Select
-            widgetProps={{ values: DROP_DOWN_OPTIONS }}
+            widgetProps={{ options: DROP_DOWN_OPTIONS }}
             value={joinType}
             onChange={joinTypeChange}
+            disabled={disabled}
           />
           <If condition={joinType === 'Outer'}>
             <div className={classes.checkboxesGroup}>
@@ -173,6 +175,7 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
                     <FormControlLabel
                       className={classes.checkbox}
                       key={`${i}-${input.name}`}
+                      disabled={disabled}
                       control={
                         <Checkbox
                           checked={input.selected}
@@ -208,8 +211,4 @@ export default function JoinTypeWidget(props: IJoinTypeWidgetProps) {
   );
 }
 
-(JoinTypeWidget as any).propTypes = {
-  value: PropTypes.string,
-  inputSchema: PropTypes.object,
-  onChange: PropTypes.func,
-};
+(JoinTypeWidget as any).propTypes = WIDGET_PROPS;

cdap-ui/app/cdap/components/AbstractWidget/MemorySelectWidget/index.js
@@ -16,8 +16,9 @@
 
 import React from 'react';
 import SelectWithOptions from 'components/SelectWithOptions';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { objectQuery } from 'services/helpers';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
 require('./MemorySelectWidget.scss');
 
 export default function MemorySelectWidget({ ...props }) {
@@ -35,5 +36,5 @@ export default function MemorySelectWidget({ ...props }) {
   );
 }
 
-MemorySelectWidget.propTypes = WIDGET_PROPTYPES;
+MemorySelectWidget.propTypes = WIDGET_PROPS;
 MemorySelectWidget.defaultProps = DEFAULT_WIDGET_PROPS;

cdap-ui/app/cdap/components/AbstractWidget/MemoryTextbox/index.js
@@ -15,8 +15,9 @@
 */
 
 import React from 'react';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { Input } from 'reactstrap';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
 
 require('./MemoryTextbox.scss');
 
@@ -50,5 +51,5 @@ export default function MemoryTextbox({ ...props }) {
   );
 }
 
-MemoryTextbox.propTypes = WIDGET_PROPTYPES;
+MemoryTextbox.propTypes = WIDGET_PROPS;
 MemoryTextbox.defaultProps = DEFAULT_WIDGET_PROPS;

cdap-ui/app/cdap/components/AbstractWidget/SecureKey/index.js
@@ -16,7 +16,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { MySecureKeyApi } from 'api/securekey';
 import { getCurrentNamespace } from 'services/NamespaceStore';
 import IconSVG from 'components/IconSVG';
@@ -26,13 +26,14 @@ import { objectQuery } from 'services/helpers';
 import { SECURE_KEY_PREFIX, SECURE_KEY_SUFFIX, SYSTEM_NAMESPACE } from 'services/global-constants';
 import Mousetrap from 'mousetrap';
 import { Observable } from 'rxjs/Observable';
+import { WIDGET_PROPS } from 'components/AbstractWidget/constants';
 require('./SecureKeyTextarea.scss');
 
 const PREFIX = 'features.AbstractWidget.SecureKeyTextarea';
 
 export default class SecureKeyTextarea extends Component {
   static propTypes = {
-    ...WIDGET_PROPTYPES,
+    ...WIDGET_PROPS,
     inputTextType: PropTypes.oneOf(['textarea', 'text', 'password']),
   };
   static defaultProps = DEFAULT_WIDGET_PROPS;

cdapio/cdap-ui

cdap-ui/app/cdap/components/AbstractWidget/DatasetSelectorWidget/index.tsx
@@ -24,9 +24,10 @@ import classnames from 'classnames';
 import ee from 'event-emitter';
 import { objectQuery } from 'services/helpers';
 import ThemeWrapper from 'components/ThemeWrapper';
-import PropTypes from 'prop-types';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { KEY_CODE } from 'services/global-constants';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
+import { IWidgetProps } from 'components/AbstractWidget';
 
 const styles = (theme): StyleRules => {
   return {
@@ -78,26 +79,30 @@ const styles = (theme): StyleRules => {
   };
 };
 
-interface IDatasetSelector extends WithStyles<typeof styles> {
-  onChange: (value) => void;
-  value: string;
+interface IDatasetSelectorWidgetProps {
   placeholder?: string;
 }
+
+interface IDatasetSelectorProps
+  extends IWidgetProps<IDatasetSelectorWidgetProps>,
+    WithStyles<typeof styles> {}
 
 const RESULT_LIMIT = 5;
 
 export const DatasetSelectedEvent = 'dataset.selected';
 
-const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
+const DatasetSelectorView: React.SFC<IDatasetSelectorProps> = ({
   value,
   onChange,
   classes,
-  placeholder,
+  widgetProps,
+  disabled,
 }) => {
   const [datasets, setDatasets] = React.useState([]);
   const [showAutocomplete, setShowAutoComplete] = React.useState(false);
   const [selection, setSelection] = React.useState(0);
   const eventEmitter = ee(ee);
+  const placeholder = objectQuery(widgetProps, 'placeholder');
 
   React.useEffect(() => {
     MyDatasetApi.list({ namespace: getCurrentNamespace() }).subscribe(setDatasets);
@@ -109,13 +114,13 @@ const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
   }
 
   function getMatch() {
-    if (!value || value.length === 0) {
+    if (!value || value.toString().length === 0) {
       return datasets.slice(0, RESULT_LIMIT);
     }
     return datasets
       .filter((dataset) => {
         const datasetName = dataset.name.toLowerCase();
-        const inputValue = value.toLowerCase();
+        const inputValue = value.toString().toLowerCase();
         return datasetName.indexOf(inputValue) !== -1;
       })
       .slice(0, RESULT_LIMIT);
@@ -192,6 +197,7 @@ const DatasetSelectorView: React.SFC<IDatasetSelector> = ({
           onKeyDown={handleKeyDown}
           onKeyPress={handleKeyPress}
           placeholder={placeholder}
+          disabled={disabled}
           onFocus={() => setShowAutoComplete(true)}
           InputProps={{
             classes: {
@@ -237,10 +243,6 @@ function DatasetSelector(props) {
     </ThemeWrapper>
   );
 }
-(DatasetSelector as any).propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-};
+(DatasetSelector as any).propTypes = WIDGET_PROPTYPES;
 
 export default DatasetSelector;

cdap-ui/app/cdap/components/AbstractWidget/FormInputs/TextBox/index.tsx
@@ -16,9 +16,11 @@
 
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import PropTypes from 'prop-types';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import ThemeWrapper from 'components/ThemeWrapper';
+import { objectQuery } from 'services/helpers';
+import { IWidgetProps } from 'components/AbstractWidget';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
 
 const styles = (theme) => {
   return {
@@ -40,19 +42,23 @@ const styles = (theme) => {
     notchedOutline: {},
   };
 };
-interface ITextBoxProps extends WithStyles<typeof styles> {
-  value: string;
-  onChange: (value: string) => void;
-  placeholder: string;
+
+interface ITextBoxWidgetProps {
+  placeholder?: string;
 }
 
-function TextBox({ value, onChange, placeholder, classes }: ITextBoxProps) {
+interface ITextBoxProps extends IWidgetProps<ITextBoxWidgetProps>, WithStyles<typeof styles> {}
+
+const TextBox: React.FC<ITextBoxProps> = ({ value, onChange, widgetProps, disabled, classes }) => {
   const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const v = event.target.value;
     if (typeof onChange === 'function') {
       onChange(v);
     }
   };
+
+  const placeholder = objectQuery(widgetProps, 'placeholder');
+
   return (
     <TextField
       fullWidth
@@ -61,12 +67,13 @@ function TextBox({ value, onChange, placeholder, classes }: ITextBoxProps) {
       onChange={onChangeHandler}
       className={classes.root}
       placeholder={placeholder}
+      disabled={disabled}
       InputProps={{
         classes,
       }}
     />
   );
-}
+};
 const StyledTextBox = withStyles(styles)(TextBox);
 export default function TextBoxWrapper(props) {
   return (
@@ -75,8 +82,5 @@ export default function TextBoxWrapper(props) {
     </ThemeWrapper>
   );
 }
-(TextBoxWrapper as any).propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-};
+
+(TextBoxWrapper as any).propTypes = WIDGET_PROPTYPES;

cdap-ui/app/cdap/components/AbstractWidget/JoinTypeWidget/index.tsx
@@ -15,17 +15,17 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import withStyles, { WithStyles, StyleRules } from '@material-ui/core/styles/withStyles';
 import isEqual from 'lodash/isEqual';
-
-import Select from 'components/AbstractWidget/SelectWidget';
+import Select from 'components/AbstractWidget/FormInputs/Select';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Paper from '@material-ui/core/Paper';
-
 import ThemeWrapper from 'components/ThemeWrapper';
 import If from 'components/If';
+import { IWidgetProps } from 'components/AbstractWidget';
+import { objectQuery } from 'services/helpers';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
 
 const styles = (theme): StyleRules => {
   return {
@@ -69,23 +69,21 @@ const styles = (theme): StyleRules => {
   };
 };
 
-interface IJoinTypeWidgetProps extends WithStyles<typeof styles> {
-  value: string;
-  inputSchema: Array<{ name: string }>;
-  onChange: (value: string) => void;
-}
+interface IJoinTypeWidgetProps extends IWidgetProps<null>, WithStyles<typeof styles> {}
 
 const DROP_DOWN_OPTIONS: string[] = ['Inner', 'Outer'];
 
 const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
   value,
-  inputSchema,
+  extraConfig,
   onChange,
+  disabled,
   classes,
 }) => {
   const [joinType, setJoinType] = useState(DROP_DOWN_OPTIONS[1]);
   const [selectedCount, setSelectedCount] = useState(0);
   const [inputs, setInputs] = useState([]);
+  const inputSchema = objectQuery(extraConfig, 'inputSchema');
 
   const formatOutput = () => {
     const outputArr = inputs.filter((schema) => schema.selected).map((schema) => schema.name);
@@ -93,11 +91,11 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
     setSelectedCount(outputArr.length);
   };
 
-  const joinTypeChange = (event) => {
-    setJoinType(event.target.value);
+  const joinTypeChange = (val) => {
+    setJoinType(val);
     setInputs(
       inputSchema.map((input) => {
-        return { name: input.name, selected: event.target.value === 'Inner' };
+        return { name: input.name, selected: val === 'Inner' };
       })
     );
   };
@@ -122,7 +120,10 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
       );
       return;
     }
-    const initialModel = value.split(',').map((input) => input.trim());
+    const initialModel = value
+      .toString()
+      .split(',')
+      .map((input) => input.trim());
     if (isEqual(initialModel, inputSchema.map((schema) => schema.name.trim()))) {
       setJoinType('Inner');
       setInputs(
@@ -155,9 +156,10 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
       <If condition={inputs.length > 0}>
         <Paper className={classes.multiCheckboxesContainer}>
           <Select
-            widgetProps={{ values: DROP_DOWN_OPTIONS }}
+            widgetProps={{ options: DROP_DOWN_OPTIONS }}
             value={joinType}
             onChange={joinTypeChange}
+            disabled={disabled}
           />
           <If condition={joinType === 'Outer'}>
             <div className={classes.checkboxesGroup}>
@@ -173,6 +175,7 @@ const JoinTypeWidgetView: React.FC<IJoinTypeWidgetProps> = ({
                     <FormControlLabel
                       className={classes.checkbox}
                       key={`${i}-${input.name}`}
+                      disabled={disabled}
                       control={
                         <Checkbox
                           checked={input.selected}
@@ -208,8 +211,4 @@ export default function JoinTypeWidget(props: IJoinTypeWidgetProps) {
   );
 }
 
-(JoinTypeWidget as any).propTypes = {
-  value: PropTypes.string,
-  inputSchema: PropTypes.object,
-  onChange: PropTypes.func,
-};
+(JoinTypeWidget as any).propTypes = WIDGET_PROPTYPES;

cdap-ui/app/cdap/components/AbstractWidget/MemorySelectWidget/index.js
@@ -16,8 +16,9 @@
 
 import React from 'react';
 import SelectWithOptions from 'components/SelectWithOptions';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { objectQuery } from 'services/helpers';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
 require('./MemorySelectWidget.scss');
 
 export default function MemorySelectWidget({ ...props }) {

cdap-ui/app/cdap/components/AbstractWidget/MemoryTextbox/index.js
@@ -15,8 +15,9 @@
 */
 
 import React from 'react';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { Input } from 'reactstrap';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
 
 require('./MemoryTextbox.scss');
 

cdap-ui/app/cdap/components/AbstractWidget/SecureKey/index.js
@@ -16,7 +16,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { WIDGET_PROPTYPES, DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
+import { DEFAULT_WIDGET_PROPS } from 'components/AbstractWidget';
 import { MySecureKeyApi } from 'api/securekey';
 import { getCurrentNamespace } from 'services/NamespaceStore';
 import IconSVG from 'components/IconSVG';
@@ -26,6 +26,7 @@ import { objectQuery } from 'services/helpers';
 import { SECURE_KEY_PREFIX, SECURE_KEY_SUFFIX, SYSTEM_NAMESPACE } from 'services/global-constants';
 import Mousetrap from 'mousetrap';
 import { Observable } from 'rxjs/Observable';
+import { WIDGET_PROPTYPES } from 'components/AbstractWidget/constants';
 require('./SecureKeyTextarea.scss');
 
 const PREFIX = 'features.AbstractWidget.SecureKeyTextarea';