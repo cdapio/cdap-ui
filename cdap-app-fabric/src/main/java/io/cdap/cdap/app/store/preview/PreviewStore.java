propainter/cdap

CDAP-15677 restore preview status

cdap-app-fabric/src/main/java/io/cdap/cdap/app/store/preview/PreviewStore.java
@@ -17,10 +17,15 @@ package io.cdap.cdap.app.store.preview;
 
 import com.google.gson.JsonElement;
 import io.cdap.cdap.api.preview.DataTracer;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.proto.id.ApplicationId;
+import io.cdap.cdap.proto.id.ProgramId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nullable;
 
 /**
  * Interface used by {@link DataTracer} to store the preview data.
@@ -52,4 +57,38 @@ public interface PreviewStore {
    * @param applicationId the id of the preview for which the data to be removed
    */
   void remove(ApplicationId applicationId);
+
+
+  /**
+   * Save the program run id associated with the preview run
+   *
+   * @param programRunId the program run id to save
+   */
+  void putProgramRunId(ProgramRunId programRunId);
+
+  /**
+   * Get the program run id associated with the preview run
+   *
+   * @param applicationId the preview id
+   * @return the program run id of the preview, null if no run has started with the preview id
+   */
+  @Nullable
+  ProgramRunId getProgramRunId(ApplicationId applicationId);
+
+  /**
+   * Put the preview status assoicated with the preview run
+   *
+   * @param applicationId the preview id
+   * @param previewStatus the preview status
+   */
+  void putPreviewStatus(ApplicationId applicationId, PreviewStatus previewStatus);
+
+  /**
+   * Get the preview status assoicated with the preview run
+   *
+   * @param applicationId the preview id
+   * @return the preview status of the preview run, null if no run has started with the preview id
+   *
+   */
+  PreviewStatus getPreviewStatus(ApplicationId applicationId);
 }

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStore.java
@@ -24,14 +24,20 @@ import io.cdap.cdap.api.common.Bytes;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.api.dataset.table.Row;
 import io.cdap.cdap.api.dataset.table.Scanner;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.app.store.preview.PreviewStore;
 import io.cdap.cdap.data2.dataset2.lib.table.MDSKey;
 import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableCore;
 import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableService;
 import io.cdap.cdap.internal.io.SchemaTypeAdapter;
+import io.cdap.cdap.proto.BasicThrowable;
+import io.cdap.cdap.proto.codec.BasicThrowableCodec;
+import io.cdap.cdap.proto.codec.EntityIdTypeAdapter;
 import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.DatasetId;
+import io.cdap.cdap.proto.id.EntityId;
 import io.cdap.cdap.proto.id.NamespaceId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -48,6 +54,8 @@ public class DefaultPreviewStore implements PreviewStore {
   private static final byte[] TRACER = Bytes.toBytes("t");
   private static final byte[] PROPERTY = Bytes.toBytes("p");
   private static final byte[] VALUE = Bytes.toBytes("v");
+  private static final byte[] RUN = Bytes.toBytes("r");
+  private static final byte[] STATUS = Bytes.toBytes("s");
 
   private final AtomicLong counter = new AtomicLong(0L);
 
@@ -124,6 +132,69 @@ public class DefaultPreviewStore implements PreviewStore {
       String message = String.format("Error while removing preview data for application '%s'.", applicationId);
       throw new RuntimeException(message, e);
     }
+  }
+
+  @Override
+  public void putProgramRunId(ProgramRunId programRunId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(EntityId.class, new EntityIdTypeAdapter()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(programRunId.getNamespace()).add(programRunId.getApplication()).build();
+    try {
+      table.put(mdsKey.getKey(), RUN, Bytes.toBytes(gson.toJson(programRunId)), 1L);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to put %s into preview store", programRunId), e);
+    }
+  }
+
+  @Override
+  public ProgramRunId getProgramRunId(ApplicationId applicationId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(EntityId.class, new EntityIdTypeAdapter()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+
+    Map<byte[], byte[]> row = null;
+    try {
+      row = table.getRow(mdsKey.getKey(), new byte[][]{RUN},
+                                             null, null, -1, null);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to get program run id for preview %s", applicationId), e);
+    }
+    if (!row.isEmpty()) {
+      return gson.fromJson(Bytes.toString(row.get(RUN)), ProgramRunId.class);
+    }
+    return null;
+  }
+
+  @Override
+  public void putPreviewStatus(ApplicationId applicationId, PreviewStatus previewStatus) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(BasicThrowable.class, new BasicThrowableCodec()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+    try {
+      table.put(mdsKey.getKey(), STATUS, Bytes.toBytes(gson.toJson(previewStatus)), 1L);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to put preview status %s for preview %s",
+                                               previewStatus, applicationId), e);
+    }
+  }
+
+  @Override
+  public PreviewStatus getPreviewStatus(ApplicationId applicationId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(BasicThrowable.class, new BasicThrowableCodec()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+
+    Map<byte[], byte[]> row = null;
+    try {
+      row = table.getRow(mdsKey.getKey(), new byte[][]{STATUS},
+                                             null, null, -1, null);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to get the preview status for preview %s", applicationId), e);
+    }
+    if (!row.isEmpty()) {
+      return gson.fromJson(Bytes.toString(row.get(STATUS)), PreviewStatus.class);
+    }
+    return null;
   }
 
   @VisibleForTesting

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStoreTest.java
@@ -19,16 +19,20 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.google.inject.Injector;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.common.app.RunIds;
 import io.cdap.cdap.internal.AppFabricTestHelper;
 import io.cdap.cdap.proto.NamespaceMeta;
+import io.cdap.cdap.proto.ProgramType;
 import io.cdap.cdap.proto.id.ApplicationId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -97,5 +101,25 @@ public class DefaultPreviewStoreTest {
     store.remove(firstApplicationId);
     firstApplicationData = store.get(firstApplicationId, "mytracer");
     Assert.assertEquals(0, firstApplicationData.size());
+  }
+
+  @Test
+  public void testPreviewInfo() throws IOException {
+    // test non existing preview
+    ApplicationId nonexist = new ApplicationId("ns1", "nonexist");
+    Assert.assertNull(store.getProgramRunId(nonexist));
+    Assert.assertNull(store.getPreviewStatus(nonexist));
+
+    // test put and get
+    ApplicationId applicationId = new ApplicationId("ns1", "app1");
+    ProgramRunId runId = new ProgramRunId("ns1", "app1", ProgramType.WORKFLOW, "test",
+                                          RunIds.generate().getId());
+    PreviewStatus status = new PreviewStatus(PreviewStatus.Status.COMPLETED, null, 0L,
+                                             System.currentTimeMillis());
+    store.putProgramRunId(runId);
+    store.putPreviewStatus(applicationId, status);
+
+    Assert.assertEquals(runId, store.getProgramRunId(applicationId));
+    Assert.assertEquals(status, store.getPreviewStatus(applicationId));
   }
 }

cdapio/cdap-ui

cdap-app-fabric/src/main/java/io/cdap/cdap/app/store/preview/PreviewStore.java
@@ -17,10 +17,15 @@ package io.cdap.cdap.app.store.preview;
 
 import com.google.gson.JsonElement;
 import io.cdap.cdap.api.preview.DataTracer;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.proto.id.ApplicationId;
+import io.cdap.cdap.proto.id.ProgramId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nullable;
 
 /**
  * Interface used by {@link DataTracer} to store the preview data.
@@ -52,4 +57,38 @@ public interface PreviewStore {
    * @param applicationId the id of the preview for which the data to be removed
    */
   void remove(ApplicationId applicationId);
+
+  /**
+   * Save the program run id associated with the preview run
+   *
+   * @param programRunId the program run id to save
+   */
+  void setProgramId(ProgramRunId programRunId);
+
+  /**
+   * Get the program run id associated with the preview run
+   *
+   * @param applicationId the preview id
+   * @return the program run id of the preview, null if no run has started with the preview id
+   */
+  @Nullable
+  ProgramRunId getProgramRunId(ApplicationId applicationId);
+
+  /**
+   * Set the preview status assoicated with the preview run
+   *
+   * @param applicationId the preview id
+   * @param previewStatus the preview status
+   */
+  void setPreviewStatus(ApplicationId applicationId, PreviewStatus previewStatus);
+
+  /**
+   * Get the preview status assoicated with the preview run
+   *
+   * @param applicationId the preview id
+   * @return the preview status of the preview run, null if no run has started with the preview id
+   *
+   */
+  @Nullable
+  PreviewStatus getPreviewStatus(ApplicationId applicationId);
 }

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStore.java
@@ -24,14 +24,20 @@ import io.cdap.cdap.api.common.Bytes;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.api.dataset.table.Row;
 import io.cdap.cdap.api.dataset.table.Scanner;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.app.store.preview.PreviewStore;
 import io.cdap.cdap.data2.dataset2.lib.table.MDSKey;
 import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableCore;
 import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableService;
 import io.cdap.cdap.internal.io.SchemaTypeAdapter;
+import io.cdap.cdap.proto.BasicThrowable;
+import io.cdap.cdap.proto.codec.BasicThrowableCodec;
+import io.cdap.cdap.proto.codec.EntityIdTypeAdapter;
 import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.DatasetId;
+import io.cdap.cdap.proto.id.EntityId;
 import io.cdap.cdap.proto.id.NamespaceId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -48,6 +54,8 @@ public class DefaultPreviewStore implements PreviewStore {
   private static final byte[] TRACER = Bytes.toBytes("t");
   private static final byte[] PROPERTY = Bytes.toBytes("p");
   private static final byte[] VALUE = Bytes.toBytes("v");
+  private static final byte[] RUN = Bytes.toBytes("r");
+  private static final byte[] STATUS = Bytes.toBytes("s");
 
   private final AtomicLong counter = new AtomicLong(0L);
 
@@ -124,6 +132,69 @@ public class DefaultPreviewStore implements PreviewStore {
       String message = String.format("Error while removing preview data for application '%s'.", applicationId);
       throw new RuntimeException(message, e);
     }
+  }
+
+  @Override
+  public void setProgramId(ProgramRunId programRunId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(EntityId.class, new EntityIdTypeAdapter()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(programRunId.getNamespace()).add(programRunId.getApplication()).build();
+    try {
+      table.put(mdsKey.getKey(), RUN, Bytes.toBytes(gson.toJson(programRunId)), 1L);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to put %s into preview store", programRunId), e);
+    }
+  }
+
+  @Override
+  public ProgramRunId getProgramRunId(ApplicationId applicationId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(EntityId.class, new EntityIdTypeAdapter()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+
+    Map<byte[], byte[]> row = null;
+    try {
+      row = table.getRow(mdsKey.getKey(), new byte[][]{RUN},
+                                             null, null, -1, null);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to get program run id for preview %s", applicationId), e);
+    }
+    if (!row.isEmpty()) {
+      return gson.fromJson(Bytes.toString(row.get(RUN)), ProgramRunId.class);
+    }
+    return null;
+  }
+
+  @Override
+  public void setPreviewStatus(ApplicationId applicationId, PreviewStatus previewStatus) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(BasicThrowable.class, new BasicThrowableCodec()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+    try {
+      table.put(mdsKey.getKey(), STATUS, Bytes.toBytes(gson.toJson(previewStatus)), 1L);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to put preview status %s for preview %s",
+                                               previewStatus, applicationId), e);
+    }
+  }
+
+  @Override
+  public PreviewStatus getPreviewStatus(ApplicationId applicationId) {
+    // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
+    Gson gson = new GsonBuilder().registerTypeAdapter(BasicThrowable.class, new BasicThrowableCodec()).create();
+    MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace()).add(applicationId.getApplication()).build();
+
+    Map<byte[], byte[]> row = null;
+    try {
+      row = table.getRow(mdsKey.getKey(), new byte[][]{STATUS},
+                                             null, null, -1, null);
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to get the preview status for preview %s", applicationId), e);
+    }
+    if (!row.isEmpty()) {
+      return gson.fromJson(Bytes.toString(row.get(STATUS)), PreviewStatus.class);
+    }
+    return null;
   }
 
   @VisibleForTesting

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStoreTest.java
@@ -19,16 +19,20 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.google.inject.Injector;
+import io.cdap.cdap.app.preview.PreviewStatus;
 import io.cdap.cdap.common.app.RunIds;
 import io.cdap.cdap.internal.AppFabricTestHelper;
 import io.cdap.cdap.proto.NamespaceMeta;
+import io.cdap.cdap.proto.ProgramType;
 import io.cdap.cdap.proto.id.ApplicationId;
+import io.cdap.cdap.proto.id.ProgramRunId;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -97,5 +101,25 @@ public class DefaultPreviewStoreTest {
     store.remove(firstApplicationId);
     firstApplicationData = store.get(firstApplicationId, "mytracer");
     Assert.assertEquals(0, firstApplicationData.size());
+  }
+
+  @Test
+  public void testPreviewInfo() throws IOException {
+    // test non existing preview
+    ApplicationId nonexist = new ApplicationId("ns1", "nonexist");
+    Assert.assertNull(store.getProgramRunId(nonexist));
+    Assert.assertNull(store.getPreviewStatus(nonexist));
+
+    // test put and get
+    ApplicationId applicationId = new ApplicationId("ns1", "app1");
+    ProgramRunId runId = new ProgramRunId("ns1", "app1", ProgramType.WORKFLOW, "test",
+                                          RunIds.generate().getId());
+    PreviewStatus status = new PreviewStatus(PreviewStatus.Status.COMPLETED, null, 0L,
+                                             System.currentTimeMillis());
+    store.setProgramId(runId);
+    store.setPreviewStatus(applicationId, status);
+
+    Assert.assertEquals(runId, store.getProgramRunId(applicationId));
+    Assert.assertEquals(status, store.getPreviewStatus(applicationId));
   }
 }