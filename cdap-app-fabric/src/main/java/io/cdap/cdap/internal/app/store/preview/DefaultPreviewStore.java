prafullkotecha/cdap

[CDAP-16754] Adding support for representing logical types as string in preview

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStore.java
@@ -21,6 +21,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.inject.Inject;
 import io.cdap.cdap.api.common.Bytes;
+import io.cdap.cdap.api.data.format.StructuredRecord;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.api.dataset.table.Row;
 import io.cdap.cdap.api.dataset.table.Scanner;
@@ -76,7 +77,8 @@ public class DefaultPreviewStore implements PreviewStore {
   @Override
   public void put(ApplicationId applicationId, String tracerName, String propertyName, Object value) {
     // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
-    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter()).create();
+    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter())
+      .registerTypeAdapter(StructuredRecord.class, new PreviewJsonSerDe()).create();
     MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace())
       .add(applicationId.getApplication()).add(tracerName).add(counter.getAndIncrement()).build();
 
@@ -94,7 +96,8 @@ public class DefaultPreviewStore implements PreviewStore {
   @Override
   public Map<String, List<JsonElement>> get(ApplicationId applicationId, String tracerName) {
     // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
-    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter()).create();
+    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter())
+      .registerTypeAdapter(StructuredRecord.class, new PreviewJsonSerDe()).create();
     byte[] startRowKey = new MDSKey.Builder().add(applicationId.getNamespace())
       .add(applicationId.getApplication()).add(tracerName).build().getKey();
     byte[] stopRowKey = new MDSKey(Bytes.stopKeyForPrefix(startRowKey)).getKey();
@@ -106,11 +109,7 @@ public class DefaultPreviewStore implements PreviewStore {
         Map<byte[], byte[]> columns = indexRow.getColumns();
         String propertyName = Bytes.toString(columns.get(PROPERTY));
         JsonElement value = gson.fromJson(Bytes.toString(columns.get(VALUE)), JsonElement.class);
-        List<JsonElement> values = result.get(propertyName);
-        if (values == null) {
-          values = new ArrayList<>();
-          result.put(propertyName, values);
-        }
+        List<JsonElement> values = result.computeIfAbsent(propertyName, k -> new ArrayList<>());
         values.add(value);
       }
     } catch (IOException e) {

cdapio/cdap-ui

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStore.java
@@ -21,6 +21,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.inject.Inject;
 import io.cdap.cdap.api.common.Bytes;
+import io.cdap.cdap.api.data.format.StructuredRecord;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.api.dataset.table.Row;
 import io.cdap.cdap.api.dataset.table.Scanner;
@@ -76,7 +77,8 @@ public class DefaultPreviewStore implements PreviewStore {
   @Override
   public void put(ApplicationId applicationId, String tracerName, String propertyName, Object value) {
     // PreviewStore is a singleton and we have to create gson for each operation since gson is not thread safe.
-    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter()).create();
+    Gson gson = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter())
+      .registerTypeAdapter(StructuredRecord.class, new PreviewJsonSerializer()).create();
     MDSKey mdsKey = new MDSKey.Builder().add(applicationId.getNamespace())
       .add(applicationId.getApplication()).add(tracerName).add(counter.getAndIncrement()).build();
 
@@ -106,11 +108,7 @@ public class DefaultPreviewStore implements PreviewStore {
         Map<byte[], byte[]> columns = indexRow.getColumns();
         String propertyName = Bytes.toString(columns.get(PROPERTY));
         JsonElement value = gson.fromJson(Bytes.toString(columns.get(VALUE)), JsonElement.class);
-        List<JsonElement> values = result.get(propertyName);
-        if (values == null) {
-          values = new ArrayList<>();
-          result.put(propertyName, values);
-        }
+        List<JsonElement> values = result.computeIfAbsent(propertyName, k -> new ArrayList<>());
         values.add(value);
       }
     } catch (IOException e) {