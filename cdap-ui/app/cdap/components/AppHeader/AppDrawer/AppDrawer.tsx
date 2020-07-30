tkosin/cdap

system delay notification

cdap-ui/app/cdap/components/AppHeader/AppDrawer/AppDrawer.tsx
@@ -140,7 +140,7 @@ class AppDrawer extends React.PureComponent<IAppDrawerProps> {
             id="navbar-home"
             isActive={location.pathname === `/cdap/${nsurl}`}
           />
-          <ExperimentalFeature name={'data-ingestion'}>
+          <ExperimentalFeature id="data-ingestion">
             <DrawerFeatureLink
               featureName="Ingest"
               featureUrl={`/${nsurl}/ingestion`}

cdap-ui/app/cdap/components/Home/index.js
@@ -197,7 +197,7 @@ export default class Home extends Component {
             render={(props) => {
               return (
                 <ToggleExperiment
-                  name="virtual-scroll-demo"
+                  id="virtual-scroll-demo"
                   defaultComponent={<Page404 {...props} />}
                   experimentalComponent={<VirtualScrollDemo />}
                 />
@@ -227,7 +227,7 @@ export default class Home extends Component {
             render={(props) => {
               return (
                 <ToggleExperiment
-                  name="data-ingestion"
+                  id="data-ingestion"
                   defaultComponent={<Page404 {...props} />}
                   experimentalComponent={<Ingestion />}
                 />

cdap-ui/app/cdap/components/Lab/LabExperimentTest/index.tsx
@@ -24,11 +24,11 @@ const DEFAULT_EXPERIMENT = 'cdap-common-experiment';
 const LabExperimentTest: React.FC = () => {
   return (
     <div>
-      <ExperimentalFeature name={DEFAULT_EXPERIMENT}>
+      <ExperimentalFeature id={DEFAULT_EXPERIMENT}>
         <p data-cy="experimental-feature-selector">This is an experimental component.</p>
       </ExperimentalFeature>
       <ToggleExperiment
-        name={DEFAULT_EXPERIMENT}
+        id={DEFAULT_EXPERIMENT}
         defaultComponent={
           <p data-cy="default-feature-toggle-selector">This is default component for the toggle.</p>
         }

cdapio/cdap-ui

cdap-ui/app/cdap/components/AppHeader/AppDrawer/AppDrawer.tsx
@@ -140,7 +140,7 @@ class AppDrawer extends React.PureComponent<IAppDrawerProps> {
             id="navbar-home"
             isActive={location.pathname === `/cdap/${nsurl}`}
           />
-          <ExperimentalFeature name={'data-ingestion'}>
+          <ExperimentalFeature experimentId="data-ingestion">
             <DrawerFeatureLink
               featureName="Ingest"
               featureUrl={`/${nsurl}/ingestion`}

cdap-ui/app/cdap/components/Home/index.js
@@ -207,7 +207,7 @@ export default class Home extends Component {
               });
               return (
                 <ToggleExperiment
-                  name="virtual-scroll-demo"
+                  experimentId="virtual-scroll-demo"
                   defaultComponent={<Page404 {...props} />}
                   experimentalComponent={<VirtualScrollDemo />}
                 />
@@ -241,7 +241,7 @@ export default class Home extends Component {
             render={(props) => {
               return (
                 <ToggleExperiment
-                  name="data-ingestion"
+                  experimentId="data-ingestion"
                   defaultComponent={<Page404 {...props} />}
                   experimentalComponent={<Ingestion />}
                 />

cdap-ui/app/cdap/components/Lab/LabExperimentTest/index.tsx
@@ -24,11 +24,11 @@ const DEFAULT_EXPERIMENT = 'cdap-common-experiment';
 const LabExperimentTest: React.FC = () => {
   return (
     <div>
-      <ExperimentalFeature name={DEFAULT_EXPERIMENT}>
+      <ExperimentalFeature experimentId={DEFAULT_EXPERIMENT}>
         <p data-cy="experimental-feature-selector">This is an experimental component.</p>
       </ExperimentalFeature>
       <ToggleExperiment
-        name={DEFAULT_EXPERIMENT}
+        experimentId={DEFAULT_EXPERIMENT}
         defaultComponent={
           <p data-cy="default-feature-toggle-selector">This is default component for the toggle.</p>
         }