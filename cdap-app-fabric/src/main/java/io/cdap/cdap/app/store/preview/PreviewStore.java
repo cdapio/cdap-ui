ardian4/cdap

[CDAP-16714] Implement TTL for the preview data.

cdap-app-fabric/src/main/java/io/cdap/cdap/app/store/preview/PreviewStore.java
@@ -121,4 +121,10 @@ public interface PreviewStore {
    */
   @Nullable
   byte[] getPreviewRequestPollerInfo(ApplicationId applicationId);
+
+  /**
+   * Deletes the preview data older than ttl.
+   * @param ttlInSeconds ttl in seconds
+   */
+  void deleteDataOlderThan(long ttlInSeconds);
 }

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/preview/PreviewDataCleanupService.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.internal.app.preview;
+
+import com.google.common.util.concurrent.AbstractScheduledService;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import io.cdap.cdap.app.preview.PreviewConfigModule;
+import io.cdap.cdap.app.store.preview.PreviewStore;
+import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableService;
+import io.cdap.cdap.internal.app.store.preview.DefaultPreviewStore;
+import org.apache.twill.common.Threads;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The clean up service that cleans up preview data periodically.
+ */
+public class PreviewDataCleanupService extends AbstractScheduledService {
+  private final PreviewStore previewStore;
+  private final long cleanUpInterval;
+  private final long ttl;
+  private ScheduledExecutorService executor;
+
+  @Inject
+  PreviewDataCleanupService(@Named(PreviewConfigModule.PREVIEW_LEVEL_DB) LevelDBTableService previewLevelDBTableService,
+                            CConfiguration cConf) {
+    this.previewStore = new DefaultPreviewStore(previewLevelDBTableService);
+    this.cleanUpInterval = cConf.getLong(Constants.Preview.DATA_CLEANUP_INTERVAL_SECONDS);
+    this.ttl = cConf.getLong(Constants.Preview.DATA_TTL_SECONDS);
+  }
+
+  @Override
+  protected final ScheduledExecutorService executor() {
+    executor = Executors.newSingleThreadScheduledExecutor(Threads.createDaemonThreadFactory("preview-cleanup"));
+    return executor;
+  }
+
+  @Override
+  protected void runOneIteration() {
+    previewStore.deleteDataOlderThan(ttl);
+  }
+
+  @Override
+  protected Scheduler scheduler() {
+    // Try right away if there's anything to cleanup, we will then schedule based on the minimum retention interval
+    return Scheduler.newFixedRateSchedule(1, cleanUpInterval, TimeUnit.SECONDS);
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    if (executor != null) {
+      executor.shutdownNow();
+    }
+  }
+}

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/preview/DefaultPreviewRequestQueueTest.java
@@ -136,6 +136,11 @@ public class DefaultPreviewRequestQueueTest {
     public byte[] getPreviewRequestPollerInfo(ApplicationId applicationId) {
       return new byte[0];
     }
+
+    @Override
+    public void deleteDataOlderThan(long ttlInSeconds) {
+
+    }
   }
 
   @Test

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStoreTest.java
@@ -171,4 +171,49 @@ public class DefaultPreviewStoreTest {
     allWaiting = store.getAllInWaitingState();
     Assert.assertEquals(0, allWaiting.size());
   }
+
+  @Test
+  public void testPreviewTTL() throws Exception {
+    PreviewConfig previewConfig = new PreviewConfig("WordCount", ProgramType.WORKFLOW, null, null);
+    AppRequest<?> testRequest = new AppRequest<>(new ArtifactSummary("test", "1.0"), null, previewConfig);
+
+    String firstApplication = RunIds.generate(System.currentTimeMillis() - 5000).getId();
+    ApplicationId firstApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), firstApplication);
+
+    String secondApplication = RunIds.generate(System.currentTimeMillis() - 4000).getId();
+    ApplicationId secondApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), secondApplication);
+
+    String thirdApplication = RunIds.generate(System.currentTimeMillis()).getId();
+    ApplicationId thirdApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), thirdApplication);
+
+    store.add(firstApplicationId, testRequest);
+    store.add(secondApplicationId, testRequest);
+    store.add(thirdApplicationId, testRequest);
+
+    // set poller info so that it gets removed from WAITING state
+    store.setPreviewRequestPollerInfo(firstApplicationId, null);
+
+    // put data for the first application
+    store.put(firstApplicationId, "mytracer", "key1", "value1");
+    store.put(firstApplicationId, "mytracer", "key1", 2);
+
+    // get the data for first application and logger name "mytracer"
+    Map<String, List<JsonElement>> firstApplicationData = store.get(firstApplicationId, "mytracer");
+    // key1 and key2 are two keys inserted for the first application.
+    Assert.assertEquals(1, firstApplicationData.size());
+    Assert.assertEquals("value1", firstApplicationData.get("key1").get(0).getAsString());
+    Assert.assertEquals(2, firstApplicationData.get("key1").get(1).getAsInt());
+
+    // secondApplication and thirdApplication are in waiting state
+    Assert.assertEquals(2, store.getAllInWaitingState().size());
+    // Delete data for firstApplication as well as secondApplication
+    store.deleteDataOlderThan(2);
+
+    firstApplicationData = store.get(firstApplicationId, "mytracer");
+    Assert.assertEquals(0, firstApplicationData.size());
+
+    // only thirdApplication should be in waiting now
+    Assert.assertEquals(1, store.getAllInWaitingState().size());
+    Assert.assertEquals(thirdApplicationId, store.getAllInWaitingState().iterator().next().getProgram().getParent());
+  }
 }

cdapio/cdap-ui

cdap-app-fabric/src/main/java/io/cdap/cdap/app/store/preview/PreviewStore.java
@@ -121,4 +121,10 @@ public interface PreviewStore {
    */
   @Nullable
   byte[] getPreviewRequestPollerInfo(ApplicationId applicationId);
+
+  /**
+   * Deletes the preview data older than ttl.
+   * @param ttlInSeconds ttl in seconds
+   */
+  void deleteExpiredData(long ttlInSeconds);
 }

cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/preview/PreviewDataCleanupService.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.internal.app.preview;
+
+import com.google.common.util.concurrent.AbstractScheduledService;
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import io.cdap.cdap.app.preview.PreviewConfigModule;
+import io.cdap.cdap.app.store.preview.PreviewStore;
+import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableService;
+import io.cdap.cdap.internal.app.store.preview.DefaultPreviewStore;
+import org.apache.twill.common.Threads;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The clean up service that cleans up preview data periodically.
+ */
+public class PreviewDataCleanupService extends AbstractScheduledService {
+  private final PreviewStore previewStore;
+  private final long cleanUpInterval;
+  private final long ttl;
+  private ScheduledExecutorService executor;
+
+  @Inject
+  PreviewDataCleanupService(@Named(PreviewConfigModule.PREVIEW_LEVEL_DB) LevelDBTableService previewLevelDBTableService,
+                            CConfiguration cConf) {
+    this.previewStore = new DefaultPreviewStore(previewLevelDBTableService);
+    this.cleanUpInterval = cConf.getLong(Constants.Preview.DATA_CLEANUP_INTERVAL_SECONDS);
+    this.ttl = cConf.getLong(Constants.Preview.DATA_TTL_SECONDS);
+  }
+
+  @Override
+  protected final ScheduledExecutorService executor() {
+    executor = Executors.newSingleThreadScheduledExecutor(Threads.createDaemonThreadFactory("preview-cleanup"));
+    return executor;
+  }
+
+  @Override
+  protected void runOneIteration() {
+    previewStore.deleteExpiredData(ttl);
+  }
+
+  @Override
+  protected Scheduler scheduler() {
+    // Try right away if there's anything to cleanup, we will then schedule based on the minimum retention interval
+    return Scheduler.newFixedRateSchedule(1, cleanUpInterval, TimeUnit.SECONDS);
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    if (executor != null) {
+      executor.shutdownNow();
+    }
+  }
+}

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/preview/DefaultPreviewRequestQueueTest.java
@@ -136,6 +136,11 @@ public class DefaultPreviewRequestQueueTest {
     public byte[] getPreviewRequestPollerInfo(ApplicationId applicationId) {
       return new byte[0];
     }
+
+    @Override
+    public void deleteExpiredData(long ttlInSeconds) {
+
+    }
   }
 
   @Test

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/store/preview/DefaultPreviewStoreTest.java
@@ -171,4 +171,49 @@ public class DefaultPreviewStoreTest {
     allWaiting = store.getAllInWaitingState();
     Assert.assertEquals(0, allWaiting.size());
   }
+
+  @Test
+  public void testPreviewTTL() throws Exception {
+    PreviewConfig previewConfig = new PreviewConfig("WordCount", ProgramType.WORKFLOW, null, null);
+    AppRequest<?> testRequest = new AppRequest<>(new ArtifactSummary("test", "1.0"), null, previewConfig);
+
+    String firstApplication = RunIds.generate(System.currentTimeMillis() - 5000).getId();
+    ApplicationId firstApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), firstApplication);
+
+    String secondApplication = RunIds.generate(System.currentTimeMillis() - 4000).getId();
+    ApplicationId secondApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), secondApplication);
+
+    String thirdApplication = RunIds.generate(System.currentTimeMillis()).getId();
+    ApplicationId thirdApplicationId = new ApplicationId(NamespaceMeta.DEFAULT.getName(), thirdApplication);
+
+    store.add(firstApplicationId, testRequest);
+    store.add(secondApplicationId, testRequest);
+    store.add(thirdApplicationId, testRequest);
+
+    // set poller info so that it gets removed from WAITING state
+    store.setPreviewRequestPollerInfo(firstApplicationId, null);
+
+    // put data for the first application
+    store.put(firstApplicationId, "mytracer", "key1", "value1");
+    store.put(firstApplicationId, "mytracer", "key1", 2);
+
+    // get the data for first application and logger name "mytracer"
+    Map<String, List<JsonElement>> firstApplicationData = store.get(firstApplicationId, "mytracer");
+    // key1 and key2 are two keys inserted for the first application.
+    Assert.assertEquals(1, firstApplicationData.size());
+    Assert.assertEquals("value1", firstApplicationData.get("key1").get(0).getAsString());
+    Assert.assertEquals(2, firstApplicationData.get("key1").get(1).getAsInt());
+
+    // secondApplication and thirdApplication are in waiting state
+    Assert.assertEquals(2, store.getAllInWaitingState().size());
+    // Delete data for firstApplication as well as secondApplication
+    store.deleteExpiredData(2);
+
+    firstApplicationData = store.get(firstApplicationId, "mytracer");
+    Assert.assertEquals(0, firstApplicationData.size());
+
+    // only thirdApplication should be in waiting now
+    Assert.assertEquals(1, store.getAllInWaitingState().size());
+    Assert.assertEquals(thirdApplicationId, store.getAllInWaitingState().iterator().next().getProgram().getParent());
+  }
 }