5496de25a2ae30ffb5adfe452705a7610e8fa61e
Ajai <ajainarayanan@gmail.com>
2021-04-14 22:36:47 -0700

    Adds new connection create user flow.
    - This includes two steps,
    
      - Listing all connectors by categories
      - Upon choosing a connector, a connection configuration form to create connection.
    
    - The changes include basic create flow for happy path
    - Adds basic API layer to talk to ConnectionsService
    - Removes existing Create Connection flow and adds plumbing for new connection create

---
+++ app/cdap/components/Connections/Create/reducer.ts
@@ -0,0 +1,304 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { Dispatch } from 'react';
+import { IAction } from 'services/redux-helpers';
+import { ConnectionsApi } from 'api/connections';
+import { getCurrentNamespace } from 'services/NamespaceStore';
+import VersionStore from 'services/VersionStore';
+import { IArtifactObj } from 'components/PipelineContextMenu/PipelineTypes';
+import { Observable } from 'rxjs/Observable';
+
+interface ICategories {
+  name: string;
+}
+export enum ICreateConnectionSteps {
+  CONNECTOR_SELECTION = 'ConnectorSelection',
+  CONNECTOR_CONFIG = 'ConnectionConfiguration',
+  CONNECTOR_LIST = 'ConnectionList',
+}
+export interface ICreateConnectionState {
+  categories: ICategories[];
+  categoriesToConnectorsMap: Map<string, any[]>;
+  allConnectorsPluginProperties: Record<string, Pick<IConnectorDetails, keyof IConnectorDetails>>;
+  selectedConnector: string;
+  activeStep: ICreateConnectionSteps;
+  error: string;
+}
+
+export interface IConnectorDetails {
+  connectorProperties: Record<string, any>;
+  connectorWidgetJSON: unknown;
+  connectorDoc: unknown;
+}
+
+enum ICreateConnectionActions {
+  INIT,
+  SET_CATEGORIES,
+  SET_SELECTED_CONNECTOR,
+  SET_CATEGORIES_CONNECTIONS_MAP,
+  NAVIGATE_TO_CONNECTIONS_CONFIG_STEP,
+  NAVIGATE_TO_CONNECTIONS_SELECTION_STEP,
+  NAVIGATE_TO_CONNECTIONS_LIST,
+  ERROR,
+}
+
+export const initialState: ICreateConnectionState = {
+  categories: null,
+  selectedConnector: null,
+  categoriesToConnectorsMap: null,
+  allConnectorsPluginProperties: null,
+  error: null,
+  activeStep: ICreateConnectionSteps.CONNECTOR_SELECTION,
+} as const;
+
+export function reducer(state: ICreateConnectionState, action: IAction<ICreateConnectionActions>) {
+  switch (action.type) {
+    case ICreateConnectionActions.INIT:
+      return {
+        ...state,
+        categories: action.payload.categories,
+        categoriesToConnectorsMap: action.payload.categoriesToConnectorsMap,
+        allConnectorsPluginProperties: action.payload.allConnectorsPluginProperties,
+      };
+    case ICreateConnectionActions.SET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload.categories,
+      };
+    case ICreateConnectionActions.SET_SELECTED_CONNECTOR:
+      return {
+        ...state,
+        selectedConnector: action.payload.selectedConnector,
+      };
+    case ICreateConnectionActions.SET_CATEGORIES_CONNECTIONS_MAP:
+      return {
+        ...state,
+        categories: action.payload.categories,
+        categoriesToConnectorsMap: action.payload.categoriesToConnectorsMap,
+      };
+    case ICreateConnectionActions.ERROR:
+      return {
+        ...initialState,
+        error: action.payload.error,
+      };
+    case ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_CONFIG_STEP:
+      return {
+        ...state,
+        selectedConnector: action.payload.selectedConnector,
+        activeStep: ICreateConnectionSteps.CONNECTOR_CONFIG,
+      };
+    case ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_SELECTION_STEP:
+      return {
+        ...state,
+        connectionProperties: null,
+        activeStep: ICreateConnectionSteps.CONNECTOR_SELECTION,
+      };
+    case ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_LIST:
+      return {
+        ...initialState,
+        activeStep: ICreateConnectionSteps.CONNECTOR_LIST,
+      };
+    default:
+      return state;
+  }
+}
+
+export function navigateToConfigStep(dispatch, connector) {
+  dispatch({
+    type: ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_CONFIG_STEP,
+    payload: {
+      activeStep: ICreateConnectionSteps.CONNECTOR_CONFIG,
+      selectedConnector: connector,
+    },
+  });
+}
+
+export function navigateToConnectionCategoryStep(dispatch) {
+  dispatch({
+    type: ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_SELECTION_STEP,
+    payload: {
+      activeStep: ICreateConnectionSteps.CONNECTOR_SELECTION,
+      connectionProperties: null,
+    },
+  });
+}
+
+export function navigateToConnectionList(dispatch) {
+  dispatch({
+    type: ICreateConnectionActions.NAVIGATE_TO_CONNECTIONS_LIST,
+  });
+}
+
+export function fetchConnectors() {
+  const cdapVersion = VersionStore.getState().version;
+  return ConnectionsApi.listConnectors({
+    namespace: getCurrentNamespace(),
+    datapipelineArtifactVersion: cdapVersion,
+  }).toPromise();
+}
+
+export function fetchAllConnectorPluginProperties(connectors) {
+  const namespace = getCurrentNamespace();
+  const allArtifacts = connectors.map((plugin) => ({
+    ...plugin.artifact,
+    properties: [`widgets.${plugin.name}-${plugin.type}`, `doc.${plugin.name}-${plugin.type}`],
+  }));
+  return ConnectionsApi.fetchAllConnectorWidgetJSONAndDoc(
+    {
+      namespace,
+    },
+    allArtifacts
+  ).toPromise();
+}
+
+export function getCategoriesToConnectionsMap(connectionTypes = []) {
+  const categoryToConnectionsMap = new Map();
+  if (!connectionTypes.length) {
+    return categoryToConnectionsMap;
+  }
+  for (const connectionType of connectionTypes) {
+    const { category } = connectionType;
+    if (!categoryToConnectionsMap.has(category)) {
+      categoryToConnectionsMap.set(category, [connectionType]);
+      continue;
+    }
+    const existingConnections = categoryToConnectionsMap.get(category);
+    categoryToConnectionsMap.set(category, [...existingConnections, connectionType]);
+  }
+  return categoryToConnectionsMap;
+}
+
+export async function fetchConnectionDetails({
+  selectedConnectionType,
+  artifactObj,
+}: {
+  selectedConnectionType: string;
+  artifactObj?: IArtifactObj;
+}) {
+  const connDetails: IConnectorDetails = {
+    connectorProperties: null,
+    connectorWidgetJSON: null,
+    connectorDoc: null,
+  };
+  const {
+    name: artifactname = 'google-cloud',
+    version: artifactversion = '0.17.0-SNAPSHOT',
+    scope: artifactscope = 'SYSTEM',
+  } = artifactObj || {};
+  const cdapVersion = VersionStore.getState().version;
+  const connectionProperties$ = ConnectionsApi.fetchConnectorPluginProperties({
+    namespace: getCurrentNamespace(),
+    datapipelineArtifactVersion: cdapVersion,
+    connectionTypeName: 'BigQueryTable', // selectedConnectionType,
+    artifactname,
+    artifactversion,
+    artifactscope,
+  });
+  const widgetJSONKey = 'widgets.BigQueryTable-batchsource';
+  const docKey = 'doc.BigQueryTable-batchsource';
+  const connectionArtifactObj = {
+    namespace: getCurrentNamespace(),
+    artifactname,
+    artifactversion,
+    scope: artifactscope,
+  };
+  // TODO: Once we have APIs from backend to get all widget json and docs for
+  // connectors we already build a map of it. We should just use that.
+  // This is ONLY a temporary workaround to do something in create view.
+  const connectionWidgetJSON$ = ConnectionsApi.fetchConnectorArtifactProperty({
+    ...connectionArtifactObj,
+    keys: widgetJSONKey,
+  });
+  const connectionDoc$ = ConnectionsApi.fetchConnectorArtifactProperty({
+    ...connectionArtifactObj,
+    keys: docKey,
+  });
+  try {
+    const [connectionProperties, connectionWidgetJSON, connectionDoc] = await Observable.forkJoin(
+      connectionProperties$,
+      connectionWidgetJSON$,
+      connectionDoc$
+    ).toPromise();
+    connDetails.connectorProperties = connectionProperties[0].properties;
+    connDetails.connectorWidgetJSON = JSON.parse(connectionWidgetJSON[widgetJSONKey]);
+    connDetails.connectorDoc = connectionDoc[docKey];
+  } catch (e) {
+    console.log('Error fetching widget json or parsing: ', e);
+  }
+  return connDetails;
+}
+
+function getPluginKey(properties) {
+  if (!properties || !Object.keys(properties).length) {
+    return;
+  }
+  return Object.keys(properties)
+    .pop()
+    .split('.')
+    .pop();
+}
+
+function getArtifactKey(artifact) {
+  return `${artifact.name}-${artifact.version}-${artifact.scope}`;
+}
+
+function getMapOfConnectorToPluginProperties(allConnectors) {
+  const mapOfConnectorPluginProperties = {};
+  for (const connectorProps of allConnectors) {
+    const pluginKey = getPluginKey(connectorProps.properties);
+    const artifactKey = getArtifactKey(connectorProps);
+    mapOfConnectorPluginProperties[`${pluginKey}-${artifactKey}`] = connectorProps.properties;
+  }
+  return mapOfConnectorPluginProperties;
+}
+
+export async function initStore(dispatch: Dispatch<IAction<ICreateConnectionActions>>) {
+  try {
+    const connectors = await fetchConnectors();
+    const allConnectorsPluginProperties = await fetchAllConnectorPluginProperties(connectors);
+    const mapOfConnectorPluginProperties = getMapOfConnectorToPluginProperties(
+      allConnectorsPluginProperties
+    );
+    const categoriesToConnectorsMap = getCategoriesToConnectionsMap(connectors as any);
+    const categories = Array.from(new Set(connectors.map((conn) => conn.category)));
+    dispatch({
+      type: ICreateConnectionActions.INIT,
+      payload: {
+        categories,
+        categoriesToConnectorsMap,
+        allConnectorsPluginProperties: mapOfConnectorPluginProperties,
+      },
+    });
+  } catch (e) {
+    dispatch({
+      type: ICreateConnectionActions.ERROR,
+      payload: {
+        error: e.message,
+      },
+    });
+  }
+}
+
+export async function createConnection(name, connectionConfiguration) {
+  const createConnection$ = await ConnectionsApi.createConnection(
+    {
+      context: getCurrentNamespace(),
+      connectionid: name,
+    },
+    connectionConfiguration
+  ).toPromise();
+}
