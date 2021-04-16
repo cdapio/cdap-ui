f37010df9c9b227ddcf912b2669cf9d8c589a274
Edwin Elia <edwinelia@google.com>
2021-04-16 13:27:26 -0600

    [CDAP-17924] Redesign namespace admin

---
+++ app/cdap/components/NamespaceAdmin/Metrics/index.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import MetricColumn from 'components/NamespaceAdmin/Metrics/MetricColumn';
+import { connect } from 'react-redux';
+
+const useStyle = makeStyles((theme) => {
+  const border = `1px solid ${theme.palette.grey[100]}`;
+
+  return {
+    root: {
+      borderTop: border,
+      borderBottom: border,
+      paddingTop: '15px',
+      paddingBottom: '15px',
+    },
+    content: {
+      display: 'grid',
+      gridTemplateColumns: '3fr 2fr 2fr 2fr 2fr',
+
+      '& > div:not(:last-child)': {
+        borderRight: `1px solid ${theme.palette.grey[300]}`,
+      },
+    },
+    entitySection: {
+      display: 'grid',
+      gridTemplateColumns: '50% 50%',
+    },
+  };
+});
+
+interface IMetricsProps {
+  pipelinesCount: number;
+  datasetsCount: number;
+  profilesCount: number;
+  preferencesCount: number;
+}
+
+const MetricsView: React.FC<IMetricsProps> = ({
+  pipelinesCount,
+  datasetsCount,
+  profilesCount,
+  preferencesCount,
+}) => {
+  const classes = useStyle();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.content}>
+        <div className={classes.entitySection}>
+          <MetricColumn title="pipelines" metric={pipelinesCount} />
+          <MetricColumn title="datasets" metric={datasetsCount} />
+        </div>
+        <MetricColumn title="profiles" metric={profilesCount} />
+        <MetricColumn title="preferences" metric={preferencesCount} />
+        <MetricColumn title="connections" metric={15} />
+        <MetricColumn title="drivers" metric={5} />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    pipelinesCount: state.pipelinesCount,
+    datasetsCount: state.datasetsCount,
+    profilesCount: state.profilesCount,
+    preferencesCount: state.preferences.length,
+  };
+};
+
+const Metrics = connect(mapStateToProps)(MetricsView);
+export default Metrics;
---
+++ app/cdap/components/NamespaceAdmin/index.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React, { useEffect } from 'react';
+import makeStyle from '@material-ui/core/styles/makeStyles';
+import { EntityTopPanel } from 'components/EntityTopPanel';
+import { getCurrentNamespace } from 'services/NamespaceStore';
+import {
+  getNamespaceDetail,
+  getComputeProfiles,
+  reset,
+  getPreferences,
+} from 'components/NamespaceAdmin/store/ActionCreator';
+import { Provider } from 'react-redux';
+import Store from 'components/NamespaceAdmin/store';
+import Description from 'components/NamespaceAdmin/Description';
+import Metrics from 'components/NamespaceAdmin/Metrics';
+import Tabs from 'components/NamespaceAdmin/Tabs';
+
+const useStyle = makeStyle(() => {
+  return {
+    content: {
+      padding: '0 50px',
+    },
+  };
+});
+
+const NamespaceAdmin: React.FC = () => {
+  const namespace = getCurrentNamespace();
+  const classes = useStyle();
+
+  useEffect(() => {
+    getNamespaceDetail(namespace);
+    getComputeProfiles(namespace);
+    getPreferences(namespace);
+
+    return () => {
+      reset();
+    };
+  }, []);
+
+  return (
+    <Provider store={Store}>
+      <div>
+        <EntityTopPanel title={`Namespace '${namespace}'`} />
+        <div className={classes.content}>
+          <Description />
+          <Metrics />
+          <Tabs />
+        </div>
+      </div>
+    </Provider>
+  );
+};
+
+export default NamespaceAdmin;
