8c8e72c6f4896c56c5f46b8f56854aa1105bab10
Nick Briggs <njbriggs@google.com>
2021-05-03 10:30:48 -0700

    Add sample button components with new appearance

---
+++ app/cdap/components/Mui/RaisedNeutralButton/RaisedNeutralButton.stories.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import * as React from 'react';
+
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
+import RaisedNeutralButton from './index';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+
+storiesOf('Mui Raised Neutral Button', module)
+  .addDecorator(withKnobs)
+  .add(
+    'Show',
+    withInfo({
+      text: 'Show the primary button',
+    })(() => (
+      <RaisedNeutralButton
+        disabled={boolean('Disabled', false)}
+        onClick={action('click')}
+      >
+        { text('Label', 'Primary action button') }
+      </RaisedNeutralButton>
+    ))
+  )
+  .add(
+    'With icon',
+    withInfo({
+      text: 'Primary button with icon',
+    })(() => (
+      <RaisedNeutralButton
+        disabled={boolean('Disabled', false)}
+        startIcon={ <CloudUploadIcon /> }
+        onClick={action('click')}
+      >
+        { text('Label', 'Secondary action button') }
+      </RaisedNeutralButton>
+    ))
+  );
---
+++ app/cdap/components/Mui/RaisedNeutralButton/index.tsx
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React from 'react';
+import {Button} from '@material-ui/core';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyle = makeStyles((theme) => {
+  // TODO Get from theme
+  return {
+    contained: {
+      backgroundColor: '#ffffff',
+      color: '#3367d6',
+      fontFamily: 'Roboto',
+      fontSize: '13px',
+      letterSpacing: 'normal',
+      lineHeight: '32px',
+      padding: '0 12px',
+      '&$disabled': {
+        backgroundColor: '#ffffff',
+        color: 'rgba(0, 0, 0, .54)'
+      },
+      '&:hover': {
+        backgroundColor: '#ffffff',
+      }
+    },
+    disabled: {},
+  };
+});
+
+interface IRaisedNeutralButtonProps {
+  disabled: boolean;
+  onClick: () => any;
+  startIcon?: any;
+}
+
+const RaisedNeutralButton: React.FC<IRaisedNeutralButtonProps> = (props) => {
+  const classes = useStyle();
+  return <Button
+    classes={classes}
+    disabled={props.disabled}
+    onClick={props.onClick}
+    startIcon={props.startIcon}
+    variant="contained">
+    {props.children}
+  </Button>;
+}
+
+export default RaisedNeutralButton;
