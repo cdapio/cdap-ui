e3c55e669941eed4e5abf875665a1c5458dc3cca
Nick Briggs <njbriggs@google.com>
2021-05-26 16:32:54 -0700

    [CDAP-18020] Add entitiy count to new connection browser This is intentionally generic to match the use cases for the browser

---
+++ app/cdap/components/Connections/Browser/GenericBrowser/EntityCount.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import * as React from 'react';
+import makeStyle from '@material-ui/core/styles/makeStyles';
+import Tooltip from '@material-ui/core/Tooltip';
+import capitalize from 'lodash/capitalize';
+import If from 'components/If';
+
+const useStyle = makeStyle((theme) => {
+  return {
+    tooltip: {
+      backgroundColor: theme.palette.grey[50],
+    },
+    iconContainer: {
+      marginRight: '.5rem',
+    },
+  };
+});
+
+export default function EntityCount({
+  entityCounts,
+  isFiltered,
+  isTruncated,
+  totalUnfilteredCount,
+  truncationLimit,
+}) {
+  const classes = useStyle();
+
+  const entityKeys = Object.keys(entityCounts);
+  const entityCountString = entityKeys.length
+    ? entityKeys
+        .sort()
+        .map((k) => `${capitalize(k)}: ${entityCounts[k]}`)
+        .join(', ')
+    : '0';
+
+  const fullString = isFiltered
+    ? `${entityCountString} shown of ${totalUnfilteredCount} total`
+    : entityCountString;
+
+  return (
+    <span>
+      <If condition={isTruncated}>
+        <span className={classes.iconContainer}>
+          <Tooltip title={`Results limited to ${truncationLimit} entities`} classes={classes}>
+            <span className="fa fa-lg fa-exclamation-circle" />
+          </Tooltip>
+        </span>
+      </If>
+      {fullString}
+    </span>
+  );
+}
