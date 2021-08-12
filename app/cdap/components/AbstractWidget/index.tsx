4c7c756a1963fa71b98a09dbf91362d2e617116f
Sean Podesta <seanpodesta@gmail.com>
2021-08-12 11:34:14 -0700

    Merge pull request #171 from cdapio/bugfix-ui/CDAP-18338-9
    CDAP 18339 - check if use connection is true before using connection name in plugin connection browser

--- app/cdap/components/AbstractWidget/index.tsx
+++ app/cdap/components/AbstractWidget/index.tsx
@@ -21,7 +21,7 @@ import { IErrorObj } from 'components/ConfigurationGroup/utilities';
 import StateWrapper from 'components/AbstractWidget/StateWrapper';
 require('./AbstractWidget.scss');
 
-export const DEFAULT_WIDGET_PROPS = {
+export const DEFAULT_WIDGET_PROPS: IAbstractWidgetProps = {
   widgetProps: {},
   value: '',
   disabled: false,
@@ -44,7 +44,7 @@ export interface IWidgetExtraConfig {
 
 export interface IWidgetProps<T = any> {
   widgetProps?: T;
-  value: string | number;
+  value?: string | number;
   onChange?: (value) => void | React.Dispatch<any>;
   onBlur?: (value) => void | React.Dispatch<any>;
   onKeyPress?: (event: React.KeyboardEvent) => void;
@@ -59,29 +59,37 @@ export interface IWidgetProps<T = any> {
 }
 
 interface IAbstractWidgetProps extends IWidgetProps {
-  type: string;
+  type?: string;
 }
 
-export default class AbstractWidget extends React.PureComponent<IAbstractWidgetProps> {
-  public static defaultProps = DEFAULT_WIDGET_PROPS;
+const AbstractWidget = ({
+  dataCy,
+  disabled,
+  errors,
+  extraConfig,
+  onChange,
+  type,
+  updateAllProperties,
+  value,
+  widgetProps,
+}: IAbstractWidgetProps = DEFAULT_WIDGET_PROPS) => {
+  const Comp = AbstractWidgetFactory[type];
 
-  public render() {
-    const Comp = AbstractWidgetFactory[this.props.type];
+  return (
+    <div className="abstract-widget-wrapper">
+      <StateWrapper
+        comp={Comp}
+        onChange={onChange}
+        updateAllProperties={updateAllProperties}
+        value={value}
+        widgetProps={widgetProps}
+        extraConfig={extraConfig}
+        disabled={disabled}
+        errors={errors}
+        dataCy={dataCy}
+      />
+    </div>
+  );
+};
 
-    return (
-      <div className={`abstract-widget-wrapper`}>
-        <StateWrapper
-          comp={Comp}
-          onChange={this.props.onChange}
-          updateAllProperties={this.props.updateAllProperties}
-          value={this.props.value}
-          widgetProps={this.props.widgetProps}
-          extraConfig={this.props.extraConfig}
-          disabled={this.props.disabled}
-          errors={this.props.errors}
-          dataCy={this.props.dataCy}
-        />
-      </div>
-    );
-  }
-}
+export default AbstractWidget;
