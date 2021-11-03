d92cf3ef137494ddb49ad9574cd88535a2ccd6d3
Sean Podesta <podesta@google.com>
2021-11-03 14:53:53 -0700

    connected the front end and backend - still needs to fix ui styling on the add transform/delete transform buttons and connect the table validation as well as a few minor ui things

---
+++ app/cdap/components/Replicator/Create/Content/SelectColumnsWithTransforms/addToTransforms.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { IAddColumnsToTransforms, ITransformInformation } from 'components/Replicator/types';
+
+export interface IAddToTransforms {
+  transformInfo: ITransformInformation;
+  addColumnsToTransforms: (opts: IAddColumnsToTransforms) => void;
+}
+
+export const addTinkToTransforms = (transform: IAddToTransforms): void => {
+  const { tableName, columnName, directive } = transform.transformInfo;
+  const actualDirective = `TINK ${columnName} ${directive}`;
+  const columnTransformation = {
+    directive: actualDirective,
+    columnName,
+  };
+  transform.addColumnsToTransforms({ tableName, columnTransformation });
+};
+
+export const addRenameToTransforms = (transform: IAddToTransforms): void => {
+  const { tableName, columnName, directive } = transform.transformInfo;
+  const actualDirective = `rename ${columnName} ${directive}`;
+  const columnTransformation = {
+    directive: actualDirective,
+    columnName,
+  };
+  transform.addColumnsToTransforms({ tableName, columnTransformation });
+};
