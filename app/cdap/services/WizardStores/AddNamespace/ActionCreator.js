8c509b75a94f32e54b055c628aecd403ba475c04
Mehrdad Rahanjam <mrahanjam@google.com>
2022-01-31 12:04:28 -0800

    more name updates

--- app/cdap/services/WizardStores/AddNamespace/ActionCreator.js
+++ app/cdap/services/WizardStores/AddNamespace/ActionCreator.js
@@ -38,16 +38,16 @@ const createOrEditNamespace = (api) => {
     putParams['description'] = state.general.description;
   }
 
-  if (state.mapping.hbaseNamespace) {
-    putParams['config']['hbase.namespace'] = state.mapping.hbaseNamespace;
+  if (state.hadoopMapping.hbaseNamespace) {
+    putParams['config']['hbase.namespace'] = state.hadoopMapping.hbaseNamespace;
   }
 
-  if (state.mapping.hiveDatabaseName) {
-    putParams['config']['hive.database'] = state.mapping.hiveDatabaseName;
+  if (state.hadoopMapping.hiveDatabaseName) {
+    putParams['config']['hive.database'] = state.hadoopMapping.hiveDatabaseName;
   }
 
-  if (state.mapping.hdfsDirectory) {
-    putParams['config']['root.directory'] = state.mapping.hdfsDirectory;
+  if (state.hadoopMapping.hdfsDirectory) {
+    putParams['config']['root.directory'] = state.hadoopMapping.hdfsDirectory;
   }
 
   if (state.security.keyTab) {
@@ -58,24 +58,24 @@ const createOrEditNamespace = (api) => {
     putParams['config']['principal'] = state.security.principal;
   }
 
-  if (state.mapping.schedulerQueueName) {
-    putParams['config']['scheduler.queue.name'] = state.mapping.schedulerQueueName;
+  if (state.resources.schedulerQueueName) {
+    putParams['config']['scheduler.queue.name'] = state.resources.schedulerQueueName;
   }
 
-  if (state.mapping.k8sNamespace) {
-    putParams['config']['k8s.namespace'] = state.mapping.k8sNamespace;
+  if (state.resources.k8sNamespace) {
+    putParams['config']['k8s.namespace'] = state.resources.k8sNamespace;
   }
 
-  if (state.mapping.k8sNamespaceCpuLimit) {
-    putParams['config']['k8s.namespace.cpu.limits'] = state.mapping.k8sNamespaceCpuLimit;
+  if (state.resources.k8sNamespaceCpuLimit) {
+    putParams['config']['k8s.namespace.cpu.limits'] = state.resources.k8sNamespaceCpuLimit;
   }
 
-  if (state.mapping.k8sNamespaceMemoryLimit) {
-    putParams['config']['k8s.namespace.memory.limits'] = state.mapping.k8sNamespaceMemoryLimit;
+  if (state.resources.k8sNamespaceMemoryLimit) {
+    putParams['config']['k8s.namespace.memory.limits'] = state.resources.k8sNamespaceMemoryLimit;
   }
 
-  if (state.mapping.serviceAccountEmail) {
-    putParams['config']['workload.identity.gcp.service.account.email'] = state.mapping.serviceAccountEmail;
+  if (state.resources.serviceAccountEmail) {
+    putParams['config']['workload.identity.gcp.service.account.email'] = state.resources.serviceAccountEmail;
   }
 
   return api(urlParams, putParams);
