5ff34b4e9e49f9ea873253396fe2bb22742393df
Sumeng Wang <sumengwang@google.com>
2022-04-01 16:03:37 -0700

    disabled musk option when data type is not varchar
    remove unused prop
    
    clean up syntax
    
    add an exhaust list of string types
    
    change data types to reference javaSqlTypes
    
    fix targetType not showing correctly
    
    small improvement

--- app/cdap/components/Replicator/Create/Content/SelectColumnsWithTransforms/table.tsx
+++ app/cdap/components/Replicator/Create/Content/SelectColumnsWithTransforms/table.tsx
@@ -300,7 +300,7 @@ export const renderTable = ({
                   <span></span>
                 </Grid>
                 <GridCell item xs={4}>
-                  <NoPaddingSpanLeft>{row.type.toLowerCase()}</NoPaddingSpanLeft>
+                  <NoPaddingSpanLeft>{row.targetType.toLowerCase()}</NoPaddingSpanLeft>
                 </GridCell>
               </GridCellContainer>
               <GridDividerCell item xs={2} container direction="row">
@@ -328,7 +328,6 @@ export const renderTable = ({
                     tinkEnabled={tinkEnabled}
                     currentColumnName={getCurrentColumnName(row.name)}
                     addColumnsToTransforms={addColumnsToTransforms}
-                    columns={selectedList}
                   />
                 </GridCell>
                 <GridCell item xs={6}>
