49e061b159ecf6d828ec12940d7fde8ba3af027e
Mehrdad Rahanjam <mrahanjam@google.com>
2022-04-22 11:53:01 -0700

    fix: CDAP-19131 and more

--- app/cdap/components/Administration/TetheringTabContent/TetheringTable/constants.tsx
+++ app/cdap/components/Administration/TetheringTabContent/TetheringTable/constants.tsx
@@ -19,7 +19,7 @@ import T from 'i18n-react';
 import { StyledIcon } from '../shared.styles';
 
 export const PREFIX = 'features.Administration.Tethering';
-export const DESC_COLUMN_TEMPLATE = '50px 200px 5fr 200px 1.5fr 240px 225px';
+export const DESC_COLUMN_TEMPLATE = '50px 200px 5fr 200px 1.5fr 280px 225px';
 
 export const ICONS = {
   active: {
