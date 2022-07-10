e9c430650d8e615e4125592735d9e7e5435ab62a
Siva Rajana <siva@divami.com>
2022-07-10 18:56:54 +0530

    getting rid of grid branch

--- app/cdap/components/Grid/GridKPICell.tsx
+++ app/cdap/components/Grid/GridKPICell.tsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { makeStyles, styled } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: "216px",
+    minWidth: '216px',
     // maxWidth: '202px',
-    width: "fit-content",
-    backgroundColor: "#fff",
-    padding: "10px 10px 10px 30px",
-    borderRadius: "0px",
-    display: "flex",
-    flexDirection: "column",
+    width: 'fit-content',
+    backgroundColor: '#fff',
+    padding: '10px 10px 10px 30px',
+    borderRadius: '0px',
+    display: 'flex',
+    flexDirection: 'column',
   },
   KPICell: {
-    display: "flex",
-    justifyContent: "space-between",
-    paddingBottom: "5px",
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingBottom: '5px',
   },
   posRight: {
-    lineHeight: "21px",
-    fontSize: "14px",
+    lineHeight: '21px',
+    fontSize: '14px',
     fontWeight: 400,
-    color: "red",
+    color: 'red',
   },
   posLeft: {
-    fontSize: "14px",
-    lineHeight: "21px",
+    fontSize: '14px',
+    lineHeight: '21px',
     fontWeight: 400,
-    color: "#5F6368",
+    color: '#5F6368',
   },
 });
 
 const StringIndicatorBox = styled(Box)({
-  display: "flex",
+  display: 'flex',
 });
 
 const GridKPICell = () => {
@@ -44,12 +44,12 @@ const GridKPICell = () => {
   return (
     <Card className={classes.root} variant="outlined">
       <Box className={classes.KPICell}>
-        <Typography className={classes.posLeft}>{"Missing/Null"}</Typography>
-        <Typography className={classes.posRight}>{"794"}</Typography>
+        <Typography className={classes.posLeft}>{'Missing/Null'}</Typography>
+        <Typography className={classes.posRight}>{'794'}</Typography>
       </Box>
       <Box className={classes.KPICell}>
-        <Typography className={classes.posLeft}>{"Invalid"}</Typography>
-        <Typography className={classes.posRight}>{"142"}</Typography>
+        <Typography className={classes.posLeft}>{'Invalid'}</Typography>
+        <Typography className={classes.posRight}>{'142'}</Typography>
       </Box>
     </Card>
   );
