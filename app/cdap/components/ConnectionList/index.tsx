447a423858e2d5bc4f2b147c44943a3cede6ba7a
adityadivami <aditya@divami.com>
2022-07-10 16:51:28 +0530

    moved usestyles out of component

--- app/cdap/components/ConnectionList/index.tsx
+++ app/cdap/components/ConnectionList/index.tsx
@@ -6,48 +6,48 @@ import { fetchConnectors } from 'components/Connections/Create/reducer';
 import { GetConnectionIcon, GetIcon } from './IconStore';
 import WelcomeCard from './WelcomeCard';
 
-const ConnectionContainer = () => {
-  const useStyles = makeStyles(() => ({
-    flexContainer: {
-      paddingTop: '18px',
-      display: 'flex',
-      flexWrap: 'wrap',
-      backgroundColor: '#F3F6F9',
-      width: '100%',
-      height: '100%',
-      '& > :nth-child(3n+1)': {
-        borderRight: '1px solid #E3E3E3',
-        borderBottom: '1px solid #E3E3E3',
-        width: '160px',
-      },
-      '& > :nth-child(3n+2)': {
-        borderBottom: '1px solid #E3E3E3',
-        width: '180px',
-      },
-      '& > :nth-child(3n)': {
-        borderLeft: '1px solid #E3E3E3',
-        borderBottom: '1px solid #E3E3E3',
-        width: '160px',
-      },
-      '& > :nth-last-child(1)': {
-        borderBottom: '0px',
-      },
-      '& > :nth-last-child(2)': {
-        borderBottom: '0px',
-      },
-      '& > :nth-last-child(3)': {
-        borderBottom: '0px',
-      },
+const useStyles = makeStyles(() => ({
+  flexContainer: {
+    paddingTop: '18px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    backgroundColor: '#F3F6F9',
+    width: '100%',
+    height: '100%',
+    '& > :nth-child(3n+1)': {
+      borderRight: '1px solid #E3E3E3',
+      borderBottom: '1px solid #E3E3E3',
+      width: '160px',
     },
-    dashBoard: {
-      padding: '18px 59px 18px 60px',
-      backgroundColor: '#F3F6F9',
-      maxWidth: '620px',
-      border: '0px',
-      borderRight: '1px dashed #DADCE0',
+    '& > :nth-child(3n+2)': {
+      borderBottom: '1px solid #E3E3E3',
+      width: '180px',
     },
-  }));
+    '& > :nth-child(3n)': {
+      borderLeft: '1px solid #E3E3E3',
+      borderBottom: '1px solid #E3E3E3',
+      width: '160px',
+    },
+    '& > :nth-last-child(1)': {
+      borderBottom: '0px',
+    },
+    '& > :nth-last-child(2)': {
+      borderBottom: '0px',
+    },
+    '& > :nth-last-child(3)': {
+      borderBottom: '0px',
+    },
+  },
+  dashBoard: {
+    padding: '18px 59px 18px 60px',
+    backgroundColor: '#F3F6F9',
+    maxWidth: '620px',
+    border: '0px',
+    borderRight: '1px dashed #DADCE0',
+  },
+}));
 
+const ConnectionContainer = () => {
   const classes = useStyles();
 
   const [connectionsList, setConnectionsList] = useState([]);
