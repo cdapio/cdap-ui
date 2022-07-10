62ab78265aad8f8698f8957704414f171eee9c80
Siva Rajana <siva@divami.com>
2022-07-10 18:56:54 +0530

    getting rid of grid branch

--- app/cdap/components/Grid/GridTextCell.tsx
+++ app/cdap/components/Grid/GridTextCell.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: "216px",
-    backgroundColor: "#fff",
-    padding: "5px 5px 5px 30px",
-    borderRadius: "0px",
-    display: "flex",
-    flexDirection: "column",
-    width: "fit-content",
+    minWidth: '216px',
+    backgroundColor: '#fff',
+    padding: '5px 5px 5px 30px',
+    borderRadius: '0px',
+    display: 'flex',
+    flexDirection: 'column',
+    width: 'fit-content',
   },
   pos: {
-    lineHeight: "21px",
-    fontSize: "14px",
+    lineHeight: '21px',
+    fontSize: '14px',
     fontWeight: 400,
-    color: "#5F6368",
+    color: '#5F6368',
   },
 });
 
@@ -26,7 +26,7 @@ const GridTextCell = () => {
 
   return (
     <Card className={classes.root} variant="outlined">
-      <Typography className={classes.pos}>{"Kathy Albertson"}</Typography>
+      <Typography className={classes.pos}>{'Kathy Albertson'}</Typography>
     </Card>
   );
 };
