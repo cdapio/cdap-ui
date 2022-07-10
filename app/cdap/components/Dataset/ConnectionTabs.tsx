f94dc469e62a025774176884e8c9f8e9a94c8d4c
Siva Rajana <siva@divami.com>
2022-07-10 19:18:26 +0530

    removed theme from  welcome card

---
+++ app/cdap/components/Dataset/ConnectionTabs.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import Box from '@material-ui/core/Box';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { styled, Tooltip, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  boxStyles: {
+    maxWidth: '252px',
+    background: 'linear-gradient(180deg, rgba(243, 246, 249, 0) -0.07%, #F3F6F9 22.66%)',
+    border: '1px dashed #DADCE0',
+    zIndex: 1,
+    borderTop: '1px solid #E0E0E0;',
+  },
+  tabIndicatorStyles: {
+    backgroundColor: 'white',
+    minWidth: '257.24px',
+    borderWidth: '3px',
+    borderStyle: 'solid',
+    borderImage: 'linear-gradient(to left, #4681F4, 2%,white,white,white,white,white,white) 1',
+    zIndex: 2,
+  },
+  labelStyles: {
+    maxWidth: '125px',
+    fontSize: '16px',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  iconBoxStyles: {
+    width: 30,
+    height: 30,
+    boxSizing: 'border-box',
+  },
+});
+
+const StyledTab = styled(Tab)({
+  minWidth: '161px',
+  maxHeight: '54px',
+  padding: '15px 0px 15px 32px',
+  textTransform: 'none',
+  color: 'black',
+  fontSize: '16px',
+  minHeight: '53px !important',
+  '& .MuiTab-labelIcon': { minHeight: '54px !important' },
+  '& .MuiTab-wrapper': {
+    fontSize: '16px',
+    fontWeight: '400',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    gap: '9.41px',
+    flexDirection: 'row',
+    zIndex: 3,
+    whiteSpace: 'nowrap',
+  },
+  '&.MuiTab-labelIcon .MuiTab-wrapper > *:first-child': {
+    marginBottom: '0px',
+  },
+});
+
+const ConnectionsTabs = ({ connectorTypes, handleChange, value }) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.boxStyles}>
+      <TabsComponent handleChange={handleChange} value={value} connectorTypes={connectorTypes} />
+    </Box>
+  );
+};
+
+const TabsComponent = ({ handleChange, value, connectorTypes }) => {
+  const classes = useStyles();
+  return (
+    <Tabs
+      onChange={handleChange}
+      value={value}
+      orientation="vertical"
+      variant="scrollable"
+      TabIndicatorProps={{
+        className: classes.tabIndicatorStyles,
+      }}
+    >
+      {connectorTypes.map((connectorType, connectorTypeIndex) => (
+        <StyledTab
+          label={<TooltipLabel label={connectorType.name} count={connectorType.count} />}
+          value={connectorType.name}
+          icon={<Box className={classes.iconBoxStyles}>{connectorType.SVG}</Box>}
+          disableTouchRipple
+          key={`${connectorType.name}=${connectorTypeIndex}`}
+        />
+      ))}
+    </Tabs>
+  );
+};
+
+const TooltipLabel = ({ label, count }) => {
+  const classes = useStyles();
+
+  return (
+    <Tooltip title={label}>
+      <>
+        <Typography variant="body1" className={classes.labelStyles}>
+          {label}
+        </Typography>
+        <Typography variant="body1" className={classes.labelStyles}>{`(${count})`}</Typography>
+      </>
+    </Tooltip>
+  );
+};
+
+export default ConnectionsTabs;
---
+++ app/cdap/components/Dataset/SVGs/AllConnectionsIcon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const AllConnectionsIcon = () => {
+  return (
+    <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.5249 19.9653C17.1128 22.3774 13.2021 22.3774 10.79 19.9653C8.37798 17.5533 8.37798 13.6425 10.79 11.2305"
+        stroke="#4681F4"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+      <circle
+        cx="17.6533"
+        cy="13.102"
+        r="3.41177"
+        transform="rotate(45 17.6533 13.102)"
+        stroke="#4BAF4F"
+        strokeWidth="2"
+      />
+      <path
+        d="M20.7724 9.98245L26.3877 4.36719"
+        stroke="#4BAF4F"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+      <path
+        d="M5.17478 25.5806L10.79 19.9653"
+        stroke="#4681F4"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+export default AllConnectionsIcon;
