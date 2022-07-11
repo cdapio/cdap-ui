500f7eb28ebecfc811501e9fa8e8bef137dc62e8
adityadivami <aditya@divami.com>
2022-07-11 11:52:27 +0530

    dataset wrapper component issues with gghj etc. connections except for file connectortype

--- app/cdap/components/Dataset/index.tsx
+++ app/cdap/components/Dataset/index.tsx
@@ -1,82 +1,59 @@
-import * as React from 'react';
-import ConnectionTabsCaller from './ConnectionTabsCaller';
-import { getCategorizedConnections } from '../Connections/Browser/SidePanel/apiHelpers';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import { exploreConnection } from 'components/Connections/Browser/GenericBrowser/apiHelpers';
+import { getCategorizedConnections } from 'components/Connections/Browser/SidePanel/apiHelpers';
+import * as React from 'react';
+import { useLocation } from 'react-router';
+import ConnectionTabsCaller from './ConnectionTabsCaller';
 
-import { fetchConnectors } from 'components/Connections/Create/reducer';
-
-const DatasetsListComponent = () => {
-  const [selectedConnectorData, setSelectedConnectorData] = useState([]);
-
+const DatasetWrapper = () => {
   const loc = useLocation();
-  const [initialConnectionId] = useState('');
+
   const queryParams = new URLSearchParams(loc.search);
   const pathFromUrl = queryParams.get('path') || '/';
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
-  const [currentConnection, setCurrentConnection] = useState(initialConnectionId);
 
-  const [selectedDataset, setSelectedDataset] = useState([]);
+  const [data, setData] = React.useState<any>([]);
 
-  const selectedTabValueHandler = (selectedValue) => {
-    console.log(selectedValue);
-    setCurrentConnection(selectedValue);
-  };
-  const [path, setPath] = useState(pathFromUrl);
-  const fetchEntities = async (connectionid = 'qa') => {
-    setLoading(true);
-    try {
-      Promise.all([await exploreConnection({ connectionid, path })]).then((values) => {
-        const dataArray = [];
-        values.map((each) => each.entities.forEach((eachentity) => dataArray.push(eachentity)));
-        setData(dataArray);
-      });
-    } catch (e) {
-      setError(`Failed to explore connection. Error: "${e.response}"`);
-    }
-  };
-  useEffect(() => {
-    initState();
-  }, []);
-  useEffect(() => {
-    console.log(data, 'data');
+  React.useEffect(() => {
+    console.log(data, 'this is data');
   }, [data]);
 
-  useEffect(() => {
-    console.log(currentConnection);
-  }, [currentConnection]);
-  const initState = async () => {
-    const connectorTypes = await fetchConnectors();
-    const categorizedConnections = await getCategorizedConnections();
-    console.log('Categorized connections', categorizedConnections);
-
-    connectorTypes.forEach((connectorType, index) => {
-      const connections = categorizedConnections.get(connectorType.name) || [];
-
-      connections.forEach((connection) => {
-        if (connection?.connectionId !== undefined) {
-          const newElement = {
-            connectionType: connection.connectionType,
-            datasetName: connection.connectionId,
-          };
-          console.log(newElement);
-          // console.log(connection?.connectionId);
-          // setSelectedDataset(...selectedDataset,{connectorType:connection,})
-        }
-      });
-    });
+  const selectedTabValueHandler = (selectedValue: string) => {
+    console.log('selecte value', selectedValue);
+    selectedValue !== 'All Connections' && getCategorizedConnectionsFromAPI(selectedValue);
   };
+
+  const getCategorizedConnectionsFromAPI = async (selectedValue: string) => {
+    const categorizedConnections = await getCategorizedConnections();
+    const connections = categorizedConnections.get(selectedValue) || [];
+
+    fetchEntities(connections);
+  };
+
+  const fetchEntities = async (connections) => {
+    console.log('connections', connections);
+    const connectionsUpdated = connections.map((eachConnection) =>
+      exploreConnection({ connectionid: eachConnection.connectionId, path: pathFromUrl })
+    );
+
+    try {
+      await Promise.all([await connectionsUpdated]).then((values) => {
+        values.map((each) => {
+          each.map((each2) =>
+            each2.then((response) => {
+              setData((prev: any) => ([...prev, response.entities] as any).flat());
+            })
+          );
+        });
+      });
+    } catch (e) {
+      console.log('error', e);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <ConnectionTabsCaller selectedTabValueHandler={selectedTabValueHandler} />
-      </div>
-      <div>{/* Dataset Table Component to Come here */}</div>
-    </>
+    <div>
+      <ConnectionTabsCaller selectedTabValueHandler={selectedTabValueHandler} />
+    </div>
   );
 };
 
-export default DatasetsListComponent;
+export default DatasetWrapper;
