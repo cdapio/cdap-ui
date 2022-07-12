8c9909dbeed81dbe7606b3766d478c370a43ed45
adityadivami <aditya@divami.com>
2022-07-13 00:35:31 +0530

    removed unwated cide

--- app/cdap/components/Home/index.js
+++ app/cdap/components/Home/index.js
@@ -147,7 +147,6 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        x
         <Switch>
           <Route exact path="/ns/:namespace" component={HomeActions} />
           <Route exact path="/ns/:namespace/wrangler1" component={ConnectionContainerView} />
