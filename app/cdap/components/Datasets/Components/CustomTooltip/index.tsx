357fafc49b9827b5d00a36c7a66ad751832f6137
Abhilash4269 <abhilash.haranadha@gmail.com>
2022-08-20 00:50:29 +0530

    test milestone one

--- app/cdap/components/Datasets/Components/CustomTooltip/index.tsx
+++ app/cdap/components/Datasets/Components/CustomTooltip/index.tsx
@@ -1,14 +1,14 @@
-import { Box, Tooltip, TooltipProps } from '@material-ui/core';
-import * as React from 'react';
-import { useStyles } from './styles';
+import { Box, Tooltip, TooltipProps } from "@material-ui/core";
+import * as React from "react";
+import { useStyles } from "./styles";
 
 const CustomTooltip = (props: TooltipProps) => {
   const classes = useStyles();
 
   return (
-      <Box>
-                <Tooltip arrow classes={classes} {...props} />
-        </Box>
+    <Box data-testid="tooltip-parent">
+      <Tooltip arrow classes={classes} {...props} />
+    </Box>
   );
 };
 
