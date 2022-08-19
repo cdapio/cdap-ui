3094ff8e8e366d253dfef0c5ac91d71a1f631af7
Abhilash4269 <abhilash.haranadha@gmail.com>
2022-08-20 00:46:10 +0530

    one test to be covered later

--- app/cdap/components/Datasets/Components/ConnectionTabs/__test__/ConnectionTabs.test.tsx
+++ app/cdap/components/Datasets/Components/ConnectionTabs/__test__/ConnectionTabs.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ConnectionsTabs from '../index';
 
-const yolo = [{showTabs:true}]
+const tabsTestData = [{showTabs:true}]
 
 test('renders Connections Tab Component', () => {
-  render(<ConnectionsTabs tabsData={yolo} handleChange='qwe' value='apple' index='one' />);
+  render(<ConnectionsTabs tabsData={tabsTestData} handleChange={() => null} value='apple' index='one' />);
   const ele = screen.getByTestId(/connections-tabs-parent/i);
   expect(ele).toBeInTheDocument();
 });
--- app/cdap/components/Datasets/Components/CustomTooltip/index.tsx
+++ app/cdap/components/Datasets/Components/CustomTooltip/index.tsx
@@ -1,11 +1,15 @@
-import { Tooltip, TooltipProps } from '@material-ui/core';
+import { Box, Tooltip, TooltipProps } from '@material-ui/core';
 import * as React from 'react';
 import { useStyles } from './styles';
 
 const CustomTooltip = (props: TooltipProps) => {
   const classes = useStyles();
 
-  return <Tooltip arrow classes={classes} {...props} />;
+  return (
+      <Box>
+                <Tooltip arrow classes={classes} {...props} />
+        </Box>
+  );
 };
 
 export default CustomTooltip;
