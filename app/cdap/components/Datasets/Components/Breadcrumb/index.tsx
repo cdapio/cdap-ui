03f5248b9942a30fd31342afa05e3b1127585cdd
sivarajana <sivarajana@Divamis-MacBook-Air-29.local>
2022-08-23 14:49:15 +0530

    12 bugs fixed and 5 pending

--- app/cdap/components/Datasets/Components/Breadcrumb/index.tsx
+++ app/cdap/components/Datasets/Components/Breadcrumb/index.tsx
@@ -1,24 +1,34 @@
-import { Typography } from '@material-ui/core';
+import { Breadcrumbs, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { useStyles } from 'components/Datasets/Components/Breadcrumb/styles';
-import { DownloadIcon, PrevPageIcon } from 'components/Datasets/iconStore';
+import { DownloadIcon, AddConnection } from 'components/Datasets/iconStore';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getCurrentNamespace } from 'services/NamespaceStore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const BreadCumb = () => {
   const classes = useStyles();
   return (
     <Box className={classes.breadCombContainer} data-testid="bread-comb-container-parent">
-      <Box className={classes.selectPrevPage}>
-        <Link to={`/ns/${getCurrentNamespace()}/home`} style={{ textDecoration: 'none' }}>
-          <PrevPageIcon />
-        </Link>
-        <Typography>Select Dataset</Typography>
+      <Box>
+        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+          <Link color="inherit" to={`/ns/${getCurrentNamespace()}/home`}>
+            Home
+          </Link>
+          <Typography color="textPrimary">Datasets</Typography>
+        </Breadcrumbs>
       </Box>
-      <Box className={classes.importData}>
-        <DownloadIcon />
-        <Typography>Import Data</Typography>
+
+      <Box className={classes.importDataContainer}>
+        <Box className={classes.importData}>
+          <AddConnection />
+          <Box className={classes.breadCrumbTyporgraphy}>Add connection</Box>
+        </Box>
+        <Box className={classes.importData}>
+          <DownloadIcon />
+          <Box className={classes.breadCrumbTyporgraphy}>Import data</Box>
+        </Box>
       </Box>
     </Box>
   );
