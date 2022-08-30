46003a386674caf828758dfaeb7221d527ce8bf4
richadivami <richa@divami.com>
2022-08-30 20:03:15 +0530

    Removing unused code

--- app/cdap/components/WrangleHome/Components/WrangleCard/index.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/index.tsx
@@ -19,19 +19,6 @@ import { fetchConnectors } from 'components/Connections/Create/reducer';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getCurrentNamespace } from 'services/NamespaceStore';
-import { BigQuery } from './iconStore/BigQuerySVG';
-import { CloudSQLMySQL } from './iconStore/CloudSQLMySQL';
-import { CloudSQLPostGreSQL } from './iconStore/CloudSQLPostGreSQL';
-import { Database } from './iconStore/Database';
-import { GCS } from './iconStore/GCS';
-import { ImportDatasetIcon } from './iconStore/ImportDatasetIcon';
-import { Kafka } from './iconStore/Kafka';
-import { MySQL } from './iconStore/MySQL';
-import { Oracle } from './iconStore/Oracle';
-import { PostgreSQL } from './iconStore/PostgreSQL';
-import { S3 } from './iconStore/S3';
-import { Spanner } from './iconStore/Spanner';
-import { SQLServer } from './iconStore/SQLServer';
 import { useStyles } from './styles';
 import { getCategoriesToConnectorsMap, getSVG } from './Components/WidgetData';
 import { fetchConnectionDetails } from 'components/Connections/Create/reducer';
@@ -88,110 +75,14 @@ const WrangleCard = () => {
       connectorTypes: connectorDataWithSvgArray,
     });
   };
-  // const getConnectorTypesNames = async () => {
-  //   let connectorTypes = await fetchConnectors();
-
-  //   connectorTypes = connectorTypes.map((connectorType) => {
-  //     if (connectorType.name === 'S3') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: S3,
-  //       };
-  //     } else if (connectorType.name === 'Database') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: Database,
-  //       };
-  //     } else if (connectorType.name === 'BigQuery') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: BigQuery,
-  //       };
-  //     } else if (connectorType.name === 'GCS') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: GCS,
-  //       };
-  //     } else if (connectorType.name === 'Spanner') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: Spanner,
-  //       };
-  //     } else if (connectorType.name === 'Kafka') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: Kafka,
-  //       };
-  //     } else if (connectorType.name === 'SQL Server') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: SQLServer,
-  //       };
-  //     } else if (connectorType.name === 'MySQL') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: MySQL,
-  //       };
-  //     } else if (connectorType.name === 'Oracle') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: Oracle,
-  //       };
-  //     } else if (connectorType.name === 'PostgreSQL') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: PostgreSQL,
-  //       };
-  //     } else if (connectorType.name === 'File') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: ImportDatasetIcon,
-  //       };
-  //     } else if (connectorType.name === 'CloudSQLMySQL') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: CloudSQLMySQL,
-  //       };
-  //     } else if (connectorType.name === 'CloudSQLPostgreSQL') {
-  //       return {
-  //         ...connectorType,
-  //         SVG: CloudSQLPostGreSQL,
-  //       };
-  //     } else {
-  //       return {
-  //         ...connectorType,
-  //         SVG: BigQuery,
-  //       };
-  //     }
-  //   });
-
-  //   connectorTypes.unshift({
-  //     name: 'Imported Datasets',
-  //     type: 'default',
-  //     category: 'default',
-  //     description: 'All Connections from the List',
-  //     artifact: {
-  //       name: 'allConnections',
-  //       version: 'local',
-  //       scope: 'local',
-  //     },
-
-  //     SVG: ImportDatasetIcon,
-  //   });
-
-  //   setState({
-  //     connectorTypes,
-  //   });
-  // };
   useEffect(() => {
     widgetData();
-    // getConnectorTypesNames();
   }, []);
   const classes = useStyles();
   const connectorTypes = state.connectorTypes;
   return (
     <Box className={classes.wrapper} data-testid="wrangle-card-parent">
-      {state.connectorTypes.map((item, index) => {
+      {connectorTypes.map((item, index) => {
         return (
           <Link
             to={`/ns/${getCurrentNamespace()}/datasources/${item.name}`}
