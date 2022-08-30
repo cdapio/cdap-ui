772ed5079d765a06bc76e956df33acf8210d121c
richadivami <richa@divami.com>
2022-08-30 19:15:24 +0530

    Adding widgets fontsize

--- app/cdap/components/WrangleHome/Components/WrangleCard/Components/WidgetData/index.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/Components/WidgetData/index.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 import React from 'react';
 import { findHighestVersion } from 'services/VersionRange/VersionUtilities';
 import reject from 'lodash/reject';
@@ -59,5 +74,11 @@ function addVersionInfo(categoryToConnectionsMap, versionsMap) {
 }
 
 export const getSVG = (dataSrc) => {
-  return dataSrc ? <Avatar src={dataSrc} variant="square" /> : <ImageOutlined fontSize="large" />;
+  return dataSrc ? (
+    <Avatar src={dataSrc} variant="square" />
+  ) : (
+    <div style={{ fontSize: '40px' }}>
+      <ImageOutlined fontSize="inherit" />
+    </div>
+  );
 };
