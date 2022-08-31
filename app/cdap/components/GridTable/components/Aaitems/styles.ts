9a88e8777145cf9007c7ff99c6d051410587ca25
sivarajana <sivarajana@Divamis-MacBook-Air-29.local>
2022-08-31 16:30:19 +0530

    styling added

--- app/cdap/components/GridTable/components/Aaitems/styles.ts
+++ app/cdap/components/GridTable/components/Aaitems/styles.ts
@@ -16,24 +16,8 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles({
-  iconContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    border: '1px solid #E0E0E0',
-    height: '48px',
-    marginTop: '0px',
-    padding: '10px',
-  },
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginLeft: '0px',
-    marginRight: '0px',
-  },
-  searchIcon: {
-    border: 'none',
-    outline: 'none',
+  listitem: {
+    marginRight: '30px',
+    height: '20px',
   },
 });
