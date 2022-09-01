7aed9fa645babd6bd94afaa9adaba525bfd1e7fa
smritiDivami <smriti@divami.com>
2022-09-01 19:48:34 +0530

    Color fixes

--- app/cdap/components/ConnectionList/Components/SubHeader/index.tsx
+++ app/cdap/components/ConnectionList/Components/SubHeader/index.tsx
@@ -1,4 +1,19 @@
 /*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+/*
  * Copyright © 2022 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -17,11 +32,12 @@
 import { Breadcrumbs, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { useStyles } from 'components/ConnectionList/Components/SubHeader/styles';
-import { DownloadIcon, AddConnection } from 'components/ConnectionList/iconStore';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getCurrentNamespace } from 'services/NamespaceStore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import SaveAltRoundedIcon from '@material-ui/icons/SaveAltRounded';
 
 const SubHeader = () => {
   const classes = useStyles();
@@ -38,11 +54,11 @@ const SubHeader = () => {
 
       <Box className={classes.importDataContainer}>
         <Box className={classes.importData}>
-          <AddConnection />
+          <AddCircleOutlineOutlinedIcon className={classes.subheadeIcon} />
           <Box className={classes.breadCrumbTyporgraphy}>Add connection</Box>
         </Box>
         <Box className={classes.importData}>
-          <DownloadIcon />
+          <SaveAltRoundedIcon className={classes.subheadeIcon} />
           <Box className={classes.breadCrumbTyporgraphy}>Import data</Box>
         </Box>
       </Box>
