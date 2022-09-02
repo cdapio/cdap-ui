62da5b9f13aacdcea4e7d0b41a1681717365e02a
gayathriredd <gayathri@divami.com>
2022-09-02 20:40:23 +0530

    WrangleHomeColorChanges

--- app/cdap/components/WrangleHome/Components/WrangleCard/styles.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/styles.tsx
@@ -15,6 +15,7 @@
  */
 
 import { makeStyles } from '@material-ui/styles';
+import { grey } from '@material-ui/core/colors';
 
 export const useStyles = makeStyles({
   wrapper: {
@@ -26,7 +27,8 @@ export const useStyles = makeStyles({
     padding: '0px 28px',
     height: '180px',
     width: '220px',
-    border: '1px solid #E3E3E3',
+    border: '1px solid',
+    borderColor: grey[300],
     borderRadius: '10px',
     boxShadow: 'none',
     display: 'flex',
