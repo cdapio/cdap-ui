68635fb0c83a772ae8f5da8f43ecda5dd6fc318a
adityadivami <aditya@divami.com>
2022-09-03 12:22:38 +0530

    updated default exports for connection list components

--- app/cdap/components/ConnectionList/Components/CustomTooltip/index.tsx
+++ app/cdap/components/ConnectionList/Components/CustomTooltip/index.tsx
@@ -18,7 +18,7 @@ import { Box, Tooltip, TooltipProps } from '@material-ui/core';
 import * as React from 'react';
 import { useStyles } from './styles';
 
-const CustomTooltip = (props: TooltipProps) => {
+export default function CustomTooltip(props: TooltipProps) {
   const classes = useStyles();
 
   return (
@@ -26,6 +26,4 @@ const CustomTooltip = (props: TooltipProps) => {
       <Tooltip arrow classes={classes} {...props} />
     </Box>
   );
-};
-
-export default CustomTooltip;
+}
