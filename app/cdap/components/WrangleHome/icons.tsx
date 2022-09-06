7bc6b8b7703ce6a3231f6beeeb5da2d584206771
smritiDivami <smriti@divami.com>
2022-09-06 15:06:30 +0530

    any+console remove

--- app/cdap/components/WrangleHome/icons.tsx
+++ app/cdap/components/WrangleHome/icons.tsx
@@ -361,7 +361,7 @@ export const HeaderImage = (
 
 export const UnderLine = (
   <svg width="67" height="2" viewBox="0 0 67 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 0H50L53 2H3L0 0Z" fill="#4681F4" />
-    <path d="M54 0H63.5L66.5 2H57L54 0Z" fill="#4681F4" />
+    <path d="M0 0H50L53 2H3L0 0Z" fill="#2196F3" />
+    <path d="M54 0H63.5L66.5 2H57L54 0Z" fill="#2196F3" />
   </svg>
 );
