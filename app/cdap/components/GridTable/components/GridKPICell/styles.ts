35cf75f80a0dd4081ce2cd5b743b6f3fd5928f33
sivarajana <aditya@divami.com>
2022-09-07 19:04:47 +0530

    MUI color added

--- app/cdap/components/GridTable/components/GridKPICell/styles.ts
+++ app/cdap/components/GridTable/components/GridKPICell/styles.ts
@@ -15,6 +15,7 @@
  */
 
 import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
 
 export const useGridKPICellStyles = makeStyles({
   root: {
@@ -48,10 +49,10 @@ export const useGridKPICellStyles = makeStyles({
     fontSize: '14px',
     lineHeight: '21px',
     fontWeight: 400,
-    color: '#E97567',
+    color: red[600],
   },
   missingClass: {
-    color: '#E97567',
+    color: red[600],
   },
   generalClass: {
     color: '#212121',
