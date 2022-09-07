a24e6b9f85fe091bcb5f140a84ece7064e0f9970
smritiDivami <smriti@divami.com>
2022-09-07 16:03:02 +0530

    PR ISSUES

--- app/cdap/components/WrangleHome/Components/OngoingDataExploration/utils.ts
+++ app/cdap/components/WrangleHome/Components/OngoingDataExploration/utils.ts
@@ -1,31 +1,32 @@
 /*
- * Copyright © 2022 Cask Data, Inc.
+ *  Copyright © 2022 Cask Data, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
  */
 
 import { ImportDatasetIcon } from '../WrangleCard/iconStore/ImportDatasetIcon';
+import { IMassagedObject } from './types';
 
-export const generateDataForExplorationCard = (olddata: any) => {
+export const generateDataForExplorationCard = (oldData) => {
   // Massaging the data to map the API response to the Ongoing Data Exploration List
   const massagedArray = [];
 
-  if (olddata && Array.isArray(olddata) && olddata.length) {
-    olddata.forEach((eachItem: any) => {
-      const childarr = [];
+  if (oldData && Array.isArray(oldData) && oldData.length) {
+    oldData.forEach((eachItem) => {
+      const childArray = [];
 
       Object.keys(eachItem).map((keys) => {
-        const obj = {} as any;
+        const obj = {} as IMassagedObject;
 
         if (keys === 'connectionName') {
           obj.icon = ImportDatasetIcon;
@@ -45,10 +46,10 @@ export const generateDataForExplorationCard = (olddata: any) => {
         } else if (keys === 'workspaceId') {
           obj.workspaceId = eachItem[keys];
         }
-        childarr.push(obj);
+        childArray.push(obj);
       });
 
-      massagedArray.push(childarr);
+      massagedArray.push(childArray);
     });
   }
 
