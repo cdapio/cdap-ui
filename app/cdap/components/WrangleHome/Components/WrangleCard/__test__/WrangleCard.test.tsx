c892661c4d4b51aeda5dea4aa94c686ebadf6863
smritiDivami <smriti@divami.com>
2022-09-30 18:41:28 +0530

    Unit Testing Milestone 2

--- app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
@@ -15,39 +15,92 @@
  */
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import WrangleCard from '../index';
+import { render } from '@testing-library/react';
+import { WrangleCard } from '..';
+import { Router, Switch, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { getCategorizedConnections } from 'components/Connections/Browser/SidePanel/apiHelpers';
 
-test('It renders Wrangler-Card ', () => {
-  jest.mock('components/Connections/Create/reducer', () => {
-    return Promise.resolve([
+const history = createBrowserHistory({
+  basename: '/',
+});
+describe('Testing the Wrangle Card Component', () => {
+  test('Should check whether WrangleCard Component is rendered or not', () => {
+    const screen = render(
+      <Router history={history}>
+        <Switch>
+          <Route>
+            <WrangleCard />
+          </Route>
+        </Switch>
+      </Router>
+    );
+    expect(screen).toBeDefined();
+  });
+
+  it('Should', async () => {
+    const categorizedConnections = await getCategorizedConnections();
+    expect(categorizedConnections).toEqual([
       {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'HeMan',
-        type: 'js',
+        key: 'PostgreSQL',
+        value: [
+          {
+            name: 'exl',
+            connectionId: 'exl',
+            connectionType: 'PostgreSQL',
+            description: '',
+            preConfigured: false,
+            isDefault: false,
+            createdTimeMillis: 1660107919860,
+            updatedTimeMillis: 1660107919860,
+            plugin: {
+              category: 'Database',
+              name: 'PostgreSQL',
+              type: 'connector',
+              properties: {
+                host: 'jenkins.divami.com',
+                port: '5432',
+                jdbcPluginName: 'postgresql',
+                database: 'exlaibias',
+                user: 'postgres',
+                password: 'divami',
+                connectionArguments: '1=1',
+              },
+              artifact: {
+                scope: 'SYSTEM',
+                name: 'postgresql-plugin',
+                version: '1.9.0-SNAPSHOT',
+              },
+            },
+          },
+        ],
       },
       {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'BatMan',
-        type: 'js',
-      },
-      {
-        artifact: { name: 'words', scope: '', version: 'ten' },
-        category: 'hello',
-        classname: 'yolo',
-        description: 'hello',
-        name: 'SuperMan',
-        type: 'js',
+        key: 'File',
+        value: [
+          {
+            name: 'mkm',
+            connectionId: 'mkm',
+            connectionType: 'File',
+            description: 'mkk',
+            preConfigured: false,
+            isDefault: false,
+            createdTimeMillis: 1660125276816,
+            updatedTimeMillis: 1660125276816,
+            plugin: {
+              category: 'File',
+              name: 'File',
+              type: 'connector',
+              properties: {},
+              artifact: {
+                scope: 'SYSTEM',
+                name: 'core-plugins',
+                version: '2.10.0-SNAPSHOT',
+              },
+            },
+          },
+        ],
       },
     ]);
   });
-  render(<WrangleCard />);
-  const ele = screen.getByTestId(/wrangle-card-parent/i);
-  expect(ele).toBeInTheDocument();
 });
