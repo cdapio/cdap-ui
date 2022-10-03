6aca3f1a948855f4e4ddec67fcf406f535a48823
Sumeng Wang <sumengwang@google.com>
2022-10-03 13:18:21 -0700

    refactor loadingapplevel

--- app/cdap/components/shared/LoadingAppLevel/index.tsx
+++ app/cdap/components/shared/LoadingAppLevel/index.tsx
@@ -16,6 +16,7 @@
 
 import { Box, Modal } from '@material-ui/core';
 import React from 'react';
+import LoadingSVG from '../LoadingSVG';
 
 interface ILoadingAppLevelProps {
   message?: string;
@@ -42,96 +43,7 @@ export const LoadingAppLevel = ({ message, subtitle, isopen }: ILoadingAppLevelP
   return (
     <Modal open={isopen}>
       <Box sx={{ ...style }}>
-        <div>
-          <svg
-            className="loading-bar"
-            version="1.1"
-            x="0px"
-            y="0px"
-            width="24px"
-            height="30px"
-            viewBox="0 0 24 30"
-          >
-            <rect x="0" y="10" width="4" height="10" fill="#333" opacity="0.2">
-              <animate
-                attributeName="opacity"
-                attributeType="XML"
-                values="0.2; 1; .2"
-                begin="0s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="height"
-                attributeType="XML"
-                values="10; 20; 10"
-                begin="0s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="y"
-                attributeType="XML"
-                values="10; 5; 10"
-                begin="0s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-            </rect>
-            <rect x="8" y="10" width="4" height="10" fill="#333" opacity="0.2">
-              <animate
-                attributeName="opacity"
-                attributeType="XML"
-                values="0.2; 1; .2"
-                begin="0.15s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="height"
-                attributeType="XML"
-                values="10; 20; 10"
-                begin="0.15s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="y"
-                attributeType="XML"
-                values="10; 5; 10"
-                begin="0.15s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-            </rect>
-            <rect x="16" y="10" width="4" height="10" fill="#333" opacity="0.2">
-              <animate
-                attributeName="opacity"
-                attributeType="XML"
-                values="0.2; 1; .2"
-                begin="0.3s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="height"
-                attributeType="XML"
-                values="10; 20; 10"
-                begin="0.3s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-              <animate
-                attributeName="y"
-                attributeType="XML"
-                values="10; 5; 10"
-                begin="0.3s"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-            </rect>
-          </svg>
-        </div>
+        <LoadingSVG />
         <h2> {message || 'Loading...'} </h2>
         <h4>{subtitle}</h4>
       </Box>
