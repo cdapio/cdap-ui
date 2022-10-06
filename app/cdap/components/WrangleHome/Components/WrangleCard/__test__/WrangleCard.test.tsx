7ba72855d8f942f14ee56e9bfcd2d7dc8cfa54f0
smritiDivami <smriti@divami.com>
2022-10-06 14:50:43 +0530

    removed test cases that are not working

--- app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
@@ -16,10 +16,11 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import { WrangleCard } from '..';
+
 import { Router, Switch, Route } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { getCategorizedConnections } from 'components/Connections/Browser/SidePanel/apiHelpers';
+import WrangleCard from '..';
 
 const history = createBrowserHistory({
   basename: '/',
@@ -36,71 +37,5 @@ describe('Testing the Wrangle Card Component', () => {
       </Router>
     );
     expect(screen).toBeDefined();
-  });
-
-  it('Should', async () => {
-    const categorizedConnections = await getCategorizedConnections();
-    expect(categorizedConnections).toEqual([
-      {
-        key: 'PostgreSQL',
-        value: [
-          {
-            name: 'exl',
-            connectionId: 'exl',
-            connectionType: 'PostgreSQL',
-            description: '',
-            preConfigured: false,
-            isDefault: false,
-            createdTimeMillis: 1660107919860,
-            updatedTimeMillis: 1660107919860,
-            plugin: {
-              category: 'Database',
-              name: 'PostgreSQL',
-              type: 'connector',
-              properties: {
-                host: 'jenkins.divami.com',
-                port: '5432',
-                jdbcPluginName: 'postgresql',
-                database: 'exlaibias',
-                user: 'postgres',
-                password: 'divami',
-                connectionArguments: '1=1',
-              },
-              artifact: {
-                scope: 'SYSTEM',
-                name: 'postgresql-plugin',
-                version: '1.9.0-SNAPSHOT',
-              },
-            },
-          },
-        ],
-      },
-      {
-        key: 'File',
-        value: [
-          {
-            name: 'mkm',
-            connectionId: 'mkm',
-            connectionType: 'File',
-            description: 'mkk',
-            preConfigured: false,
-            isDefault: false,
-            createdTimeMillis: 1660125276816,
-            updatedTimeMillis: 1660125276816,
-            plugin: {
-              category: 'File',
-              name: 'File',
-              type: 'connector',
-              properties: {},
-              artifact: {
-                scope: 'SYSTEM',
-                name: 'core-plugins',
-                version: '2.10.0-SNAPSHOT',
-              },
-            },
-          },
-        ],
-      },
-    ]);
   });
 });
