fbcc6c3e83b5d560d5af7fc065e1ec7af1ca0768
smritiDivami <smriti@divami.com>
2022-10-10 10:30:18 +0530

    Unit Testing for Parsing Panel - Coverage  - 100 except Parsing Drawer:97%

---
+++ app/cdap/components/ParsingDrawer/__test__/ParsingDrawer.test.tsx
@@ -0,0 +1,82 @@
+/*
+ *  Copyright © 2022 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { createBrowserHistory as createHistory } from 'history';
+import { Route, Router, Switch } from 'react-router';
+import ParsingDrawer from '..';
+
+const history = createHistory({
+  basename: '/',
+});
+
+describe('It Should Test the Parsing Drawer Component', () => {
+  it('Should render the Parsing Drawer Parent Component', () => {
+    const container = render(
+      <Router history={history}>
+        <Switch>
+          <Route>
+            <ParsingDrawer />
+          </Route>
+        </Switch>
+      </Router>
+    );
+
+    expect(container).toBeDefined();
+  });
+
+  it('Should test the handleApply Button ', () => {
+    const screen = render(
+      <Router history={history}>
+        <Switch>
+          <Route>
+            <ParsingDrawer />
+          </Route>
+        </Switch>
+      </Router>
+    );
+    const handleApplyBtn = screen.getByTestId('parsing-apply-button');
+    fireEvent.click(handleApplyBtn);
+    expect(handleApplyBtn).toBeInTheDocument();
+  });
+
+  it('Should test the handleFormatChange Button ', () => {
+    const { getAllByRole, getAllByTestId, getByTestId } = render(
+      <Router history={history}>
+        <Switch>
+          <Route>
+            <ParsingDrawer />
+          </Route>
+        </Switch>
+      </Router>
+    );
+    const menu = getAllByRole('button');
+    fireEvent.mouseDown(menu[0]);
+    const option2 = getAllByTestId('input-select-1')[0];
+    fireEvent.click(option2);
+
+    fireEvent.mouseDown(menu[1]);
+    const option3 = getAllByTestId('input-select-1')[1];
+    fireEvent.click(option3);
+
+    const checkbox1 = getByTestId('parsing-checkbox-Enable quoted values');
+    fireEvent.click(checkbox1);
+
+    const checkbox2 = getByTestId('parsing-checkbox-Use first row as header');
+    fireEvent.click(checkbox2);
+  });
+});
