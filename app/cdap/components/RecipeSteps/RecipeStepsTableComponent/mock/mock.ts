624205d05b1bec6752ddae3540159555cbffaad5
abhilashDivami <abhilash@divami.com>
2022-10-13 17:07:16 +0530

    added test scripts recipe steps

---
+++ app/cdap/components/RecipeSteps/RecipeStepsTableComponent/mock/mock.ts
@@ -0,0 +1,46 @@
+export const mockRecipe = [
+  {
+    actionType: 'Parse Column',
+    description: "'Body' with delimiter 'comma' and set 'first row as header'",
+  },
+  {
+    actionType: 'a Column',
+    description: "'any'",
+  },
+  {
+    actionType: 'b Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'c Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'd Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'e Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'f Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'g Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'h Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'i Column',
+    description: "'Body'",
+  },
+  {
+    actionType: 'j',
+    description: "'Body'",
+  },
+];
