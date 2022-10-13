e59e3b113215f656c6ad1d4886b9e9a567b7c563
abhilashDivami <abhilash@divami.com>
2022-10-13 14:39:49 +0530

    formatting

--- app/cdap/components/ConnectionList/Components/TabLabelCanSample/__test__/TabLabelCanSample.test.tsx
+++ app/cdap/components/ConnectionList/Components/TabLabelCanSample/__test__/TabLabelCanSample.test.tsx
@@ -28,7 +28,6 @@ import {
 } from '../mock/mockConnectorTypeData';
 import * as apiHelpers from 'components/Connections/Browser/GenericBrowser/apiHelpers';
 
-
 const history = createBrowserHistory({
   basename: '/',
 });
