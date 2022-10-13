cccf4c3ea500ef9f37a5f015a10bf36e3a5c26e9
abhilashDivami <abhilash@divami.com>
2022-10-13 21:51:58 +0530

    updated test scripts

--- app/cdap/components/RecipeSteps/RecipeStepsTableComponent/__test__/RecipeStepsTableComponent.test.tsx
+++ app/cdap/components/RecipeSteps/RecipeStepsTableComponent/__test__/RecipeStepsTableComponent.test.tsx
@@ -27,54 +27,8 @@ const history = createHistory({
 });
 
 describe('It should test the Recipe Component', () => {
-  it('renders Recipe Component', () => {
-    // const mockRecipe = [
-    //   {
-    //     actionType: 'Parse Column',
-    //     description: "'Body' with delimiter 'comma' and set 'first row as header'",
-    //   },
-    //   {
-    //     actionType: 'a Column',
-    //     description: "'any'",
-    //   },
-    //   {
-    //     actionType: 'b Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'c Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'd Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'e Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'f Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'g Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'h Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'i Column',
-    //     description: "'Body'",
-    //   },
-    //   {
-    //     actionType: 'j',
-    //     description: "'Body'",
-    //   },
-    // ];
-    const container = render(
+  it('renders Recipe Component and triggers button and following functionality', () => {
+   const container = render(
       <Router history={history}>
         <Switch>
           <Route>
