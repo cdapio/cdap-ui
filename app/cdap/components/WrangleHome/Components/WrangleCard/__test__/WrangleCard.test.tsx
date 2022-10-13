fc50192e44ab93fd528de7eec1308800c31be9f2
abhilashDivami <abhilash@divami.com>
2022-10-13 14:31:28 +0530

    formatting

--- app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
+++ app/cdap/components/WrangleHome/Components/WrangleCard/__test__/WrangleCard.test.tsx
@@ -14,20 +14,20 @@
  * the License.
  */
 
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import WrangleCard from "../index";
-import * as reducers from "components/Connections/Create/reducer";
-import * as apiHelpers from "components/Connections/Browser/SidePanel/apiHelpers";
-import { Route, Router, Switch } from "react-router";
-import { createBrowserHistory as createHistory } from "history";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import WrangleCard from '../index';
+import * as reducers from 'components/Connections/Create/reducer';
+import * as apiHelpers from 'components/Connections/Browser/SidePanel/apiHelpers';
+import { Route, Router, Switch } from 'react-router';
+import { createBrowserHistory as createHistory } from 'history';
 
 const history = createHistory({
-  basename: "/",
+  basename: '/',
 });
 
-describe("Testing the Wrangle Card Component", () => {
-  test("Should check whether WrangleCard Component is rendered or not", () => {
+describe('Testing the Wrangle Card Component', () => {
+  test('Should check whether WrangleCard Component is rendered or not', () => {
     const container = render(
       <Router history={history}>
         <Switch>
@@ -40,266 +40,254 @@ describe("Testing the Wrangle Card Component", () => {
     expect(container).toBeDefined();
   });
 
-  test("It renders Wrangler-Card ", async () => {
-    jest.mock("components/Connections/Create/reducer", () => {
+  test('It renders Wrangler-Card ', async () => {
+    jest.mock('components/Connections/Create/reducer', () => {
       return Promise.resolve([
         {
-          artifact: { name: "words", scope: "", version: "ten" },
-          category: "hello",
-          classname: "yolo",
-          description: "hello",
-          name: "HeMan",
-          type: "js",
+          artifact: { name: 'words', scope: '', version: 'ten' },
+          category: 'hello',
+          classname: 'yolo',
+          description: 'hello',
+          name: 'HeMan',
+          type: 'js',
         },
         {
-          artifact: { name: "words", scope: "", version: "ten" },
-          category: "hello",
-          classname: "yolo",
-          description: "hello",
-          name: "BatMan",
-          type: "js",
+          artifact: { name: 'words', scope: '', version: 'ten' },
+          category: 'hello',
+          classname: 'yolo',
+          description: 'hello',
+          name: 'BatMan',
+          type: 'js',
         },
         {
-          artifact: { name: "words", scope: "", version: "ten" },
-          category: "hello",
-          classname: "yolo",
-          description: "hello",
-          name: "SuperMan",
-          type: "js",
+          artifact: { name: 'words', scope: '', version: 'ten' },
+          category: 'hello',
+          classname: 'yolo',
+          description: 'hello',
+          name: 'SuperMan',
+          type: 'js',
         },
       ]);
     });
-    jest.spyOn(reducers, "fetchConnectors").mockReturnValue(
+    jest.spyOn(reducers, 'fetchConnectors').mockReturnValue(
       Promise.resolve([
         {
-          name: "S3",
-          type: "connector",
-          category: "Amazon Web Services",
-          description: "Connection to access data in Amazon S3.",
-          className: "io.cdap.plugin.aws.s3.connector.S3Connector",
+          name: 'S3',
+          type: 'connector',
+          category: 'Amazon Web Services',
+          description: 'Connection to access data in Amazon S3.',
+          className: 'io.cdap.plugin.aws.s3.connector.S3Connector',
           artifact: {
-            name: "amazon-s3-plugins",
-            version: "1.18.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'amazon-s3-plugins',
+            version: '1.18.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "CloudSQLMySQL",
-          type: "connector",
-          category: "Database",
+          name: 'CloudSQLMySQL',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in CloudSQL MySQL Server databases using JDBC.',
+          className: 'io.cdap.plugin.cloudsql.mysql.CloudSQLMySQLConnector',
+          artifact: {
+            name: 'cloudsql-mysql-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
+          },
+        },
+        {
+          name: 'CloudSQLPostgreSQL',
+          type: 'connector',
+          category: 'Database',
           description:
-            "Connection to access data in CloudSQL MySQL Server databases using JDBC.",
-          className: "io.cdap.plugin.cloudsql.mysql.CloudSQLMySQLConnector",
+            'Connection to access data in CloudSQL PostgreSQL Server databases using JDBC.',
+          className: 'io.cdap.plugin.cloudsql.postgres.CloudSQLPostgreSQLConnector',
           artifact: {
-            name: "cloudsql-mysql-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'cloudsql-postgresql-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "CloudSQLPostgreSQL",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in CloudSQL PostgreSQL Server databases using JDBC.",
-          className:
-            "io.cdap.plugin.cloudsql.postgres.CloudSQLPostgreSQLConnector",
+          name: 'File',
+          type: 'connector',
+          category: 'File',
+          description: 'Connection to browse and sample data from the local file system.',
+          className: 'io.cdap.plugin.batch.connector.FileConnector',
           artifact: {
-            name: "cloudsql-postgresql-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'core-plugins',
+            version: '2.10.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "File",
-          type: "connector",
-          category: "File",
-          description:
-            "Connection to browse and sample data from the local file system.",
-          className: "io.cdap.plugin.batch.connector.FileConnector",
+          name: 'Database',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in relational databases using JDBC.',
+          className: 'io.cdap.plugin.db.connector.DBConnector',
           artifact: {
-            name: "core-plugins",
-            version: "2.10.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'database-plugins',
+            version: '2.10.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "Database",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in relational databases using JDBC.",
-          className: "io.cdap.plugin.db.connector.DBConnector",
+          name: 'BigQuery',
+          type: 'connector',
+          category: 'Google Cloud Platform',
+          description: 'Connection to access data in BigQuery datasets and tables.',
+          className: 'io.cdap.plugin.gcp.bigquery.connector.BigQueryConnector',
           artifact: {
-            name: "database-plugins",
-            version: "2.10.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'google-cloud',
+            version: '0.21.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "BigQuery",
-          type: "connector",
-          category: "Google Cloud Platform",
-          description:
-            "Connection to access data in BigQuery datasets and tables.",
-          className: "io.cdap.plugin.gcp.bigquery.connector.BigQueryConnector",
+          name: 'GCS',
+          type: 'connector',
+          category: 'Google Cloud Platform',
+          description: 'Connection to access data in Google Cloud Storage.',
+          className: 'io.cdap.plugin.gcp.gcs.connector.GCSConnector',
           artifact: {
-            name: "google-cloud",
-            version: "0.21.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'google-cloud',
+            version: '0.21.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "GCS",
-          type: "connector",
-          category: "Google Cloud Platform",
-          description: "Connection to access data in Google Cloud Storage.",
-          className: "io.cdap.plugin.gcp.gcs.connector.GCSConnector",
+          name: 'Spanner',
+          type: 'connector',
+          category: 'Google Cloud Platform',
+          description: 'Connection to access data in Spanner databases and tables.',
+          className: 'io.cdap.plugin.gcp.spanner.connector.SpannerConnector',
           artifact: {
-            name: "google-cloud",
-            version: "0.21.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'google-cloud',
+            version: '0.21.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "Spanner",
-          type: "connector",
-          category: "Google Cloud Platform",
-          description:
-            "Connection to access data in Spanner databases and tables.",
-          className: "io.cdap.plugin.gcp.spanner.connector.SpannerConnector",
+          name: 'Kafka',
+          type: 'connector',
+          category: 'Messaging Systems',
+          description: 'Connection to access data in Kafka topics.',
+          className: 'io.cdap.plugin.connector.KafkaConnector',
           artifact: {
-            name: "google-cloud",
-            version: "0.21.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'kafka-plugins-client',
+            version: '3.1.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "Kafka",
-          type: "connector",
-          category: "Messaging Systems",
-          description: "Connection to access data in Kafka topics.",
-          className: "io.cdap.plugin.connector.KafkaConnector",
+          name: 'SQL Server',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in SQL Server databases using JDBC.',
+          className: 'io.cdap.plugin.mssql.SqlServerConnector',
           artifact: {
-            name: "kafka-plugins-client",
-            version: "3.1.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'mssql-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "SQL Server",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in SQL Server databases using JDBC.",
-          className: "io.cdap.plugin.mssql.SqlServerConnector",
+          name: 'MySQL',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in Mysql databases using JDBC.',
+          className: 'io.cdap.plugin.mysql.MysqlConnector',
           artifact: {
-            name: "mssql-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'mysql-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "MySQL",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in Mysql databases using JDBC.",
-          className: "io.cdap.plugin.mysql.MysqlConnector",
+          name: 'Oracle',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in Oracle databases using JDBC.',
+          className: 'io.cdap.plugin.oracle.OracleConnector',
           artifact: {
-            name: "mysql-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'oracle-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
         {
-          name: "Oracle",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in Oracle databases using JDBC.",
-          className: "io.cdap.plugin.oracle.OracleConnector",
+          name: 'PostgreSQL',
+          type: 'connector',
+          category: 'Database',
+          description: 'Connection to access data in PostgreSQL databases using JDBC.',
+          className: 'io.cdap.plugin.postgres.PostgresConnector',
           artifact: {
-            name: "oracle-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
-          },
-        },
-        {
-          name: "PostgreSQL",
-          type: "connector",
-          category: "Database",
-          description:
-            "Connection to access data in PostgreSQL databases using JDBC.",
-          className: "io.cdap.plugin.postgres.PostgresConnector",
-          artifact: {
-            name: "postgresql-plugin",
-            version: "1.9.0-SNAPSHOT",
-            scope: "SYSTEM",
+            name: 'postgresql-plugin',
+            version: '1.9.0-SNAPSHOT',
+            scope: 'SYSTEM',
           },
         },
       ])
     );
 
     const dummyRes = new Map();
-    dummyRes.set("PostgreSql", [
+    dummyRes.set('PostgreSql', [
       {
-        name: "TESTING",
-        connectionId: "TESTING",
-        connectionType: "PostgreSQL",
-        description: "",
+        name: 'TESTING',
+        connectionId: 'TESTING',
+        connectionType: 'PostgreSQL',
+        description: '',
         preConfigured: false,
         isDefault: false,
         createdTimeMillis: 1663766799085,
         updatedTimeMillis: 1663766799085,
         plugin: {
-          category: "Database",
-          name: "PostgreSQL",
-          type: "connector",
+          category: 'Database',
+          name: 'PostgreSQL',
+          type: 'connector',
           properties: {
-            host: "jenkins.divami.com",
-            port: "5432",
-            jdbcPluginName: "postgresql",
-            database: "exlaibias",
-            user: "postgres",
-            password: "divami",
-            connectionArguments: "1=1",
+            host: 'jenkins.divami.com',
+            port: '5432',
+            jdbcPluginName: 'postgresql',
+            database: 'exlaibias',
+            user: 'postgres',
+            password: 'divami',
+            connectionArguments: '1=1',
           },
           artifact: {
-            scope: "SYSTEM",
-            name: "postgresql-plugin",
-            version: "1.9.0-SNAPSHOT",
+            scope: 'SYSTEM',
+            name: 'postgresql-plugin',
+            version: '1.9.0-SNAPSHOT',
           },
         },
       },
     ]);
-    dummyRes.set("File", [
+    dummyRes.set('File', [
       {
-        name: "fjhgkhjl",
-        connectionId: "fjhgkhjl",
-        connectionType: "File",
-        description: "",
+        name: 'fjhgkhjl',
+        connectionId: 'fjhgkhjl',
+        connectionType: 'File',
+        description: '',
         preConfigured: false,
         isDefault: false,
         createdTimeMillis: 1663766703232,
         updatedTimeMillis: 1663766703232,
         plugin: {
-          category: "File",
-          name: "File",
-          type: "connector",
+          category: 'File',
+          name: 'File',
+          type: 'connector',
           properties: {},
           artifact: {
-            scope: "SYSTEM",
-            name: "core-plugins",
-            version: "2.10.0-SNAPSHOT",
+            scope: 'SYSTEM',
+            name: 'core-plugins',
+            version: '2.10.0-SNAPSHOT',
           },
         },
       },
     ]);
 
-    jest
-      .spyOn(apiHelpers, "getCategorizedConnections")
-      .mockReturnValue(Promise.resolve(dummyRes));
+    jest.spyOn(apiHelpers, 'getCategorizedConnections').mockReturnValue(Promise.resolve(dummyRes));
 
     render(
       <Router history={history}>
