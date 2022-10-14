5a4dac9fe161a1041092344143c752a8aca11261
abhilashDivami <abhilash@divami.com>
2022-10-14 18:22:56 +0530

    test script updated

--- app/cdap/components/GridTable/__test__/GridTable.test.tsx
+++ app/cdap/components/GridTable/__test__/GridTable.test.tsx
@@ -85,7 +85,7 @@ describe('Testing Grid Table Component', () => {
     expect(container.getByTestId('grid-table')).toBeInTheDocument();
     const ele = screen.getByTestId(/footer-panel-box-click/i);
     fireEvent.click(ele);
-    const ele2 = screen.getByTestId(/menu-component-menu-itemkeep/i);
+    const ele2 = screen.getByTestId(/menu-item-component-8/i);
     fireEvent.click(ele2);
     expect(container).toBeDefined();
    
