7ddc9816ec6c9db117e07c763ed8d5364ce1349a
abhilashDivami <abhilash@divami.com>
2022-10-28 21:55:01 +0530

    added test scripts

--- app/cdap/components/ConnectionList/Components/ConnectionTabs/index.tsx
+++ app/cdap/components/ConnectionList/Components/ConnectionTabs/index.tsx
@@ -130,7 +130,6 @@ export default function ConnectionsTabs({
                 disableTouchRipple
                 key={`${connectorType.name}=${connectorTypeIndex}`}
                 id={connectorType.name}
-                data-testid={`connection-tab-${connectorType.name}`}
                 className={index > 1 && !connectorType.canBrowse ? classes.wrangleTab : null}
               />
             ))}
