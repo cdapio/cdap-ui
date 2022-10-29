e9a890899ae276e3dc8460b345ba9ff995b8cef6
Dhruti <dhruti@divami.com>
2022-10-29 15:19:06 +0530

    chanes done in automation

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Snackbar.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Snackbar.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class Snackbar {
-    @Given("Navigate to the Home Page")
+   @Given("Navigate to the Home Page")
     public void navigateToTheHomePage() {
         SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
         WaitHelper.waitForPageToLoad();
@@ -20,56 +20,59 @@ public class Snackbar {
     @Then("Click on the Connector type")
     public void clickOnTheConnectorType(){
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("item3"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangle-card-File"));
     }
     @Then("Click on the first tab of second column")
     public void clickOnTheFirstTabOfSecondColumn() {
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connections-tab-button-EXL"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-tab-ss"));
     }
     @Then("Click on the first tab of third column")
     public void clickOnTheFirstTabOfThirdColumn() {
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connections-tab-button-information_schema"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-tab-Users"));
     }
-    @When("Hover&Click on the Wrangler")
-    public void hoverAndClickOnTheWrangler() {
+    @Then("Click on the first tab of fourth column")
+    public void clickOnTheFirstTabOfFourthColumn() {
         WaitHelper.waitForPageToLoad();
-        WebElement ele = Helper.locateElementByTestId("connections-tab-button-sql_features");
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-tab-divami"));
+    }
+    @Then("Click on the first tab of fifth column")
+    public void clickOnTheFirstTabOfFifthColumn() {
+        WaitHelper.waitForPageToLoad();
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-tab-Desktop"));
+    }
+    @When("Hover&Click on the Wrangler of first file")
+    public void hoverAndClickOnTheWranglerFirstFile() {
+        WaitHelper.waitForPageToLoad();
+        WebElement ele = Helper.locateElementByTestId("connection-tab-desktop.ini");
         Actions action = new Actions(SeleniumDriver.getDriver());
         WaitHelper.waitForPageToLoad();
         action.moveToElement(ele).perform();
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connections-tab-explore"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-list-wrangle-link"));
     }
     @Then("Check the Successful Snackbar is displayed or not & Click o the close icon")
     public void checkTheSuccessfulSnackbar(){
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-close-icon"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-success-icon"));
     }
-    @Then("Click on the Home link in Wrangle Page")
-    public void clickOnTheHomeLinkInWranglePage(){
+    @When("Hover&Click on the Wrangler next file")
+    public void hoverAndClickOnTheWranglerNextFile() {
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("breadcrumb-home-text"));
-    }
-    @Then("Click on the second tab of third column")
-    public void clickOnTheSecondTabOfTheThirdColumn(){
-        WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connections-tab-button-pg_catalog"));
-    }
-    @When("Hover&Click on the Wrangler pg_catalog")
-    public void hoverAndClickOnTheWranglerPg_Catalog() {
-        WaitHelper.waitForPageToLoad();
-        WebElement ele = Helper.locateElementByTestId("connections-tab-button-pg_aggregate_fnoid_index");
+        WebElement ele = Helper.locateElementByTestId("connection-tab-Eclipse Java");
         Actions action = new Actions(SeleniumDriver.getDriver());
         WaitHelper.waitForPageToLoad();
         action.moveToElement(ele).perform();
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("connections-tab-ref-explore"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("connection-list-wrangle-link"));
     }
     @Then("Check the Failure Snackbar is displayed or not & Click o the close icon")
     public void checkTheFailureSnackbar(){
         WaitHelper.waitForPageToLoad();
-        ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-close-icon"));
+        ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-failure-icon"));
     }
+
+
+
 }
