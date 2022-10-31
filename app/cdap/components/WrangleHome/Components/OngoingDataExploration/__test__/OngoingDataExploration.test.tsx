483ed6763721767611a709930ed715e65f53daaa
abhilashDivami <abhilash@divami.com>
2022-10-31 15:31:46 +0530

    reverted whole file to previous state

--- app/cdap/components/WrangleHome/Components/OngoingDataExploration/__test__/OngoingDataExploration.test.tsx
+++ app/cdap/components/WrangleHome/Components/OngoingDataExploration/__test__/OngoingDataExploration.test.tsx
@@ -14,28 +14,25 @@
  * the License.
  */
 
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import OngoingDataExploration from "components/WrangleHome/Components/OngoingDataExploration/index";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import OngoingDataExploration from '../index';
 
-describe("It should test OngoingDataExploration Component", () => {
-  test("renders Ongoing Data Exploration component", () => {
-    jest.mock("api/dataprep", () => {
-      return Promise.resolve([
-        {
-          connectionName: "test1",
-          workspaceName: "Divami_Users_Emails.xlsx",
-          recipeSteps: 0,
-        },
-        {
-          connectionName: "Upload",
-          workspaceName: "Divami_Users_Emails.xlsx",
-          recipeSteps: 0,
-        },
-      ]);
+const testObj = {
+  connectionName: 'Upload',
+  workspaceName: 'Divami_Users_Emails.xlsx',
+  recipeSteps: 0,
+  dataQuality: 100,
+};
+
+test('renders Ongoing Data Exploration component', () => {
+  jest.mock('api/dataprep', () => {
+    return Promise.resolve([
+      { connectionName: 'yolo', workspaceName: 'Divami_Users_Emails.xlsx', recipeSteps: 0 },
+      { connectionName: 'Upload', workspaceName: 'Divami_Users_Emails.xlsx', recipeSteps: 0 },
+    ]); 
     });
     render(<OngoingDataExploration />);
     const ele = screen.getByTestId(/ongoing-data-explore-parent/i);
     expect(ele).toBeInTheDocument();
   });
-});
