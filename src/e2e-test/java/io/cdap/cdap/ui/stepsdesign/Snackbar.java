d1efc718d25abc2a864e11fc9af4af66f7df4bdf
Dhruti <dhruti@divami.com>
2022-11-03 09:45:46 +0530

    Integration script Small changes for Snackbar

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Snackbar.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/Snackbar.java
@@ -7,30 +7,24 @@ import io.cdap.e2e.utils.SeleniumDriver;
 import io.cdap.e2e.utils.WaitHelper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.junit.Assert;
-import java.util.List;
-import org.openqa.selenium.By;
-import java.lang.Thread;
 
 public class Snackbar {
     @Given("Navigate to the Home Page")
     public void navigateToTheHomePage() {
         SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
         WaitHelper.waitForPageToLoad();
+        System.out.println("Navigated to Home Page successfully");
     }
     @Then("Click on the data exploration card")
     public void clickOnTheDataExplorationCard(){
         WaitHelper.waitForPageToLoad();
         ElementHelper.clickOnElement(Helper.locateElementByTestId("ongoing-data-exploration-card-0"));
+        System.out.println("Clicked on the Data Exploration Card");
     }
     @Then("Click on the Snackbar close icon")
     public void verifyTheSnackbarPopUpIsComingOrNot(){
         WaitHelper.waitForPageToLoad();
         ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-close-icon"));
-        System.out.println("The toast message is closed successfully");
+        System.out.println("The Snackbar closed successfully");
     }
 }
