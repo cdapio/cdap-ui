d9653314502e65ea5573adad6bfe55e19bf001f4
tharunpola <tharun@divami.com>
2022-11-09 23:29:57 +0530

    PR issue fixes

--- app/cdap/components/FooterPanel/__test__/FooterPanel.test.tsx
+++ app/cdap/components/FooterPanel/__test__/FooterPanel.test.tsx
@@ -28,7 +28,11 @@ describe('Test Footer Panel Component', () => {
       <Router history={history}>
         <Switch>
           <Route>
-            <FooterPanel recipeStepsCount={42} gridMetaInfo={{ rowCount: 66, columnCount: 6 }} />
+            <FooterPanel
+              recipeStepsCount={42}
+              gridMetaInfo={{ rowCount: 66, columnCount: 6 }}
+              setOpenColumnViewHandler={jest.fn}
+            />
           </Route>
         </Switch>
       </Router>
