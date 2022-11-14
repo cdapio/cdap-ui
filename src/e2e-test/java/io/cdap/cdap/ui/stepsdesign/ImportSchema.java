7b9703c16fd4e8524bf6ac32c51b60f70ea519a5
sudhadivami <sudhalavanya@divami.com>
2022-11-14 14:59:46 +0530

    test

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/ImportSchema.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/ImportSchema.java
@@ -7,8 +7,6 @@ import io.cdap.e2e.utils.SeleniumDriver;
 import io.cdap.e2e.utils.WaitHelper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
@@ -27,27 +25,24 @@ public class ImportSchema {
 
     @Then("Click on the wrangle button")
     public void clickOnFirstTabOfTheSecondColumn() {
-        boolean flag = true;
         try {
-            for (int i = 1; flag != false; i++) {
+            for (int i = 1; i<=10; i++) {
                 WebElement ele = Helper.locateElementByTestId("connection-tab-" + i + "0");
                 if (ElementHelper.isElementDisplayed(ele)) {
-                    System.out.println("element found");
-
+                    System.out.println("element found at index = " + i);
+                    WebElement button = Helper.locateElementByTestId("connection-tab-label-" + i + "0");
                     Actions action = new Actions(SeleniumDriver.getDriver());
-                    
-                    action.moveToElement(ele).perform();
-                    if (Helper.isElementExists("connections-tab-explore")) {
+                    action.moveToElement(button).build().perform();''''
+                    if (Helper.isElementExists("connections-tab-explore",button)) {
                         ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangle-text"));
                         System.out.println("wrangle clicked");
                         if (Helper.isElementExists("snackbar-alert")) {
                             ElementHelper.clickOnElement(Helper.locateElementByTestId("snackbar-close-icon"));
-                            break;
+                         break;
                         }
                         break;
                     } else {
                         ele.click();
-                        flag = true;
                         System.out.println("folder clicked");
                     }
                 }
