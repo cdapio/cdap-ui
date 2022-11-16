d4d8c52cb147304d65130d633400fdd2eca05697
sudhadivami <sudhalavanya@divami.com>
2022-11-16 23:31:59 +0530

    string

---
+++ src/e2e-test/features/recipestep.feature
@@ -0,0 +1,36 @@
+#
+# Copyright © 2022 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+@Integration_Tests
+Feature: Directives
+
+  @Directives
+  Scenario: Go through the directives functionality
+    Given Navigate to the Home page
+    Then Click on the Data explorations card
+    Then Verify if user is on the wrangle page
+    Then Verify if the directive panel is displayed
+    Then Enter command in the panel with the data "uppercase:body_2"
+    Then Click on 'Recipe steps' button
+    Then Verify if recipe panel is displayed
+#    Then Click on delete icon of any step
+#    Then click on download icon of recipe panel
+#    Then Verify if clicking on close icon of panel
+
+#    Then click on close icon
+#    Examples:
+#      | Id |
+#      | 12 |
---
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/RecipeStep.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.ui.stepsdesign;
+
+import io.cdap.cdap.ui.utils.Constants;
+import io.cdap.e2e.utils.ElementHelper;
+import io.cdap.e2e.utils.WaitHelper;
+import io.cdap.e2e.utils.SeleniumDriver;
+import io.cdap.cdap.ui.utils.Helper;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class RecipeStep {
+    @Given("Navigate to the Home page")
+    public void navigateToHomePage() {
+        SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
+        WaitHelper.waitForPageToLoad();
+    }
+
+    @Then("Click on the Data explorations card")
+    public void clickOnTheDataExplorationsCard() {
+        try {
+            WaitHelper.waitForPageToLoad();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangler-home-ongoing-data-exploration-card"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+
+    @Then("Verify if user is on the wrangle page")
+    public void verifyIfUserIsOnTheWranglePage() {
+        String url = SeleniumDriver.getDriver().getCurrentUrl();
+        Assert.assertTrue(url.contains("http://localhost:11011/cdap/ns/default/wrangler-grid"));
+    }
+
+    @Then("Verify if the directive panel is displayed")
+    public void directivePanelIsDisplayed() {
+        try {
+            WebElement panel = Helper.locateElementByTestId("directive-input-parent");
+            Assert.assertTrue(ElementHelper.isElementDisplayed(panel));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+
+    @Then("Enter command in the panel with the data {string}")
+    public void checkCommandFunction(String command) {
+        try {
+//            String text = Helper.locateElementByTestId("table-cell-" + id).getText();
+//            String capital = text.toUpperCase();
+            WebElement panel = Helper.locateElementByTestId("select-directive-input-search");
+            panel.click();
+            panel.sendKeys(command);
+            String check = panel.getText();
+            panel.sendKeys(Keys.ENTER);
+            SeleniumDriver.getDriver().manage().window().maximize();
+            SeleniumDriver.getDriver().navigate().refresh();
+//            WebElement ele = Helper.locateElementByTestId("table-cell-" + id);
+//            String newText = ele.getText();
+//            Assert.assertEquals(capital, newText);
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+
+    @Then("click on close icon")
+    public void closeIcon() {
+        try {
+            SeleniumDriver.getDriver().navigate().refresh();
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("close-directive-panel"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+    @Then("Click on 'Recipe steps' button")
+    public void clickOnRecipeStepButton() {
+        try {
+            ElementHelper.clickOnElement(Helper.locateElementByTestId("recipe-steps-button"));
+        } catch (Exception e) {
+            System.err.println("error:" + e);
+        }
+    }
+    @Then("Verify if recipe panel is displayed")
+    public void recipePanelIsDisplayed() {
+        Assert.assertTrue(ElementHelper.isElementDisplayed(Helper.locateElementByTestId("drawer-widget-parent")));
+    }
+}
+
