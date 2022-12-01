c64a2864cef2908f4b2ff31bd92181a9dc488f4d
abhilashDivami <abhilash@divami.com>
2022-12-01 12:05:58 +0530

    UT Fixes

--- app/cdap/components/WrangleHome/Components/OngoingDataExplorationsCard/__tests__/OngoingDataExplorationsCard.test.tsx
+++ app/cdap/components/WrangleHome/Components/OngoingDataExplorationsCard/__tests__/OngoingDataExplorationsCard.test.tsx
@@ -16,15 +16,15 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import OngoingDataExplorationCard from '../index';
-import { mockItems, mockItemsWithPercentage, mockItemsPercentageData } from '../mock/mock';
+import OngoingDataExplorationCard from 'components/WrangleHome/Components/OngoingDataExplorationsCard/index';
+import { mockItemsWithPercentage } from 'components/WrangleHome/Components/OngoingDataExplorationsCard/mock/mock';
 
 describe('Test Ongoing Data Explrations Component', () => {
   it('Should render OngoingDataExplorationCard component', () => {
     render(
       <OngoingDataExplorationCard
-        explorationCardDetails={mockItemsWithPercentage[0]}
-        cardIndex={1}
+        explorationCardDetails={mockItemsWithPercentage}
+        cardIndex={1} 
       />
     );
     const ele = screen.getByTestId('ongoing-data-explorations-card-1');
