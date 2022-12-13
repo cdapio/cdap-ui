8b79bac1dab9bf09810a904bdf7140ed6b27e5c7
Dhruti <dhruti@divami.com>
2022-12-13 15:24:50 +0530

    spaces deleted at first in java file

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/TransformationsFormat.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/TransformationsFormat.java
@@ -29,163 +29,163 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 public class TransformationsFormat {
-    @Given("Navigate to Home Page of Wrangle for Format")
-    public void navigateToTheHomePageFormat() {
-        SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
-        WaitHelper.waitForPageToLoad();
-    }
+  @Given("Navigate to Home Page of Wrangle for Format")
+  public void navigateToTheHomePageFormat() {
+    SeleniumDriver.openPage(Constants.WRANGLE_HOME_URL);
+    WaitHelper.waitForPageToLoad();
+  }
 
-    @Then("Click on the Data Explorations card for format")
-    public void clickOnTheDataExplorationCardToTestFormat() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangler-home-ongoing-data-exploration-card-0"));
-            String url = SeleniumDriver.getDriver().getCurrentUrl();
-            Assert.assertTrue(url.contains("http://localhost:11011/cdap/ns/default/wrangler-grid"));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Data Explorations card for format")
+  public void clickOnTheDataExplorationCardToTestFormat() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      ElementHelper.clickOnElement(Helper.locateElementByTestId("wrangler-home-ongoing-data-exploration-card-0"));
+      String url = SeleniumDriver.getDriver().getCurrentUrl();
+      Assert.assertTrue(url.contains("http://localhost:11011/cdap/ns/default/wrangler-grid"));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Verify if the Transformation Toolbar is displayed on the Grid Page")
-    public void verifyIfTheTransformationToolbarIsDisplayedOnTheGridPage() {
-        WaitHelper.waitForPageToLoad();
-        try {
-            boolean flag = true;
-            while (flag == true) {
-                if (Helper.isElementExists(Helper.getCssSelectorByDataTestId("loading-indicator"))) {
-                    flag = true;
-                } else {
-                    flag = false;
-                }
-            }
-            Assert.assertTrue(
-                    Helper.isElementExists(Helper.getCssSelectorByDataTestId("transformations-toolbar-container")));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
+  @Then("Verify if the Transformation Toolbar is displayed on the Grid Page")
+  public void verifyIfTheTransformationToolbarIsDisplayedOnTheGridPage() {
+    WaitHelper.waitForPageToLoad();
+    try {
+      boolean flag = true;
+      while (flag == true) {
+        if (Helper.isElementExists(Helper.getCssSelectorByDataTestId("loading-indicator"))) {
+          flag = true;
+        } else {
+          flag = false;
         }
+      }
+      Assert.assertTrue(
+              Helper.isElementExists(Helper.getCssSelectorByDataTestId("transformations-toolbar-container")));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Structure icon")
-    public void clickOnTheStructureIcon() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            ElementHelper.clickOnElement(Helper.locateElementByTestId("toolbar-icon-button-Structure"));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Structure icon")
+  public void clickOnTheStructureIcon() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      ElementHelper.clickOnElement(Helper.locateElementByTestId("toolbar-icon-button-Structure"));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Format")
-    public void clickOnTheFormat() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-text"));
-            WebElement ele = SeleniumDriver.getDriver()
-                    .findElement(By.xpath("//*[@data-testid='toolbar-icon-button-text']"));
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", ele);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Format")
+  public void clickOnTheFormat() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-text"));
+      WebElement ele = SeleniumDriver.getDriver()
+              .findElement(By.xpath("//*[@data-testid='toolbar-icon-button-text']"));
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", ele);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Uppercase")
-    public void clickOnTheUppercase() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-uppercase"));
-            WebElement ele = SeleniumDriver.getDriver().findElement(
-                    By.xpath("//*[@data-testid='toolbar-icon-button-uppercase']"));
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", ele);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Uppercase")
+  public void clickOnTheUppercase() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-uppercase"));
+      WebElement ele = SeleniumDriver.getDriver().findElement(
+              By.xpath("//*[@data-testid='toolbar-icon-button-uppercase']"));
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", ele);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the lowercase")
-    public void clickOnTheLowercase() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-lowercase"));
-            WebElement ele = SeleniumDriver.getDriver().findElement(
-                    By.xpath("//*[@data-testid='toolbar-icon-button-lowercase']"));
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", ele);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the lowercase")
+  public void clickOnTheLowercase() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-button-lowercase"));
+      WebElement ele = SeleniumDriver.getDriver().findElement(
+              By.xpath("//*[@data-testid='toolbar-icon-button-lowercase']"));
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", ele);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Title Case")
-    public void clickOnTheTitleCase() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-label-titlecase"));
-            WebElement ele = SeleniumDriver.getDriver().findElement(
-                    By.xpath("//*[@data-testid='toolbar-icon-label-titlecase']"));
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", ele);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Title Case")
+  public void clickOnTheTitleCase() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("toolbar-icon-label-titlecase"));
+      WebElement ele = SeleniumDriver.getDriver().findElement(
+              By.xpath("//*[@data-testid='toolbar-icon-label-titlecase']"));
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", ele);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Verify if the user is on the Add transformation step panel")
-    public void verifyIfTheUserIsOnTheAddTransformationStepPanel() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            Assert.assertTrue(ElementHelper.isElementDisplayed(
-                    Helper.locateElementByTestId("add-transformation-drawer")));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Verify if the user is on the Add transformation step panel")
+  public void verifyIfTheUserIsOnTheAddTransformationStepPanel() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      Assert.assertTrue(ElementHelper.isElementDisplayed(
+              Helper.locateElementByTestId("add-transformation-drawer")));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Select Column button")
-    public void clickOnTheSelectColumnButton() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            ElementHelper.clickOnElement(Helper.locateElementByTestId("select-column-button"));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Select Column button")
+  public void clickOnTheSelectColumnButton() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      ElementHelper.clickOnElement(Helper.locateElementByTestId("select-column-button"));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the radio button of any column")
-    public void clickOnTheRadioButtonOfAnyColumn() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            ElementHelper.clickOnElement(Helper.locateElementByTestId("radio-input-2"));
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the radio button of any column")
+  public void clickOnTheRadioButtonOfAnyColumn() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      ElementHelper.clickOnElement(Helper.locateElementByTestId("radio-input-2"));
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Done button")
-    public void clickOnTheDoneButton() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("button_done"));
-            WebElement ele = SeleniumDriver.getDriver().findElement(
-                    By.xpath("//*[@data-testid='button_done']"));
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", ele);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Done button")
+  public void clickOnTheDoneButton() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WaitHelper.waitForElementToBeDisplayed(Helper.locateElementByTestId("button_done"));
+      WebElement ele = SeleniumDriver.getDriver().findElement(
+              By.xpath("//*[@data-testid='button_done']"));
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", ele);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 
-    @Then("Click on the Apply Step button")
-    public void clickOnTheApplyStepButton() {
-        try {
-            WaitHelper.waitForPageToLoad();
-            WebElement element = Helper.locateElementByTestId("apply-step-button");
-            JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
-            executor.executeScript("arguments[0].click();", element);
-        } catch (Exception e) {
-            System.err.println("error:" + e);
-        }
+  @Then("Click on the Apply Step button")
+  public void clickOnTheApplyStepButton() {
+    try {
+      WaitHelper.waitForPageToLoad();
+      WebElement element = Helper.locateElementByTestId("apply-step-button");
+      JavascriptExecutor executor = (JavascriptExecutor) SeleniumDriver.getDriver();
+      executor.executeScript("arguments[0].click();", element);
+    } catch (Exception e) {
+      System.err.println("error:" + e);
     }
+  }
 }
