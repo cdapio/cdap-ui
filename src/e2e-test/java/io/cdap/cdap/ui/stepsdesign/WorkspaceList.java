065205f8e9e05f730c1c6f79042d91de958892e1
Dhruti <dhruti@divami.com>
2022-12-15 17:17:17 +0530

    deleted unwanted lines

--- src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/WorkspaceList.java
+++ src/e2e-test/java/io/cdap/cdap/ui/stepsdesign/WorkspaceList.java
@@ -16,12 +16,10 @@
 
 package io.cdap.cdap.ui.stepsdesign;
 
-import io.cdap.cdap.ui.utils.Constants;
 import io.cdap.cdap.ui.utils.Helper;
 import io.cdap.e2e.utils.ElementHelper;
 import io.cdap.e2e.utils.SeleniumDriver;
 import io.cdap.e2e.utils.WaitHelper;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
