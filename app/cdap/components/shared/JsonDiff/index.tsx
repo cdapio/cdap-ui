57334b1260b11df0825ec047bae008dd54d50c4e
Sumeng Wang <sumengwang@google.com>
2022-12-16 14:36:51 -0800

    address some comments

--- app/cdap/components/shared/JsonDiff/index.tsx
+++ app/cdap/components/shared/JsonDiff/index.tsx
@@ -65,6 +65,94 @@ interface IJsonDiffProps {
   selectedConfig: object;
 }
 
+const JsonDiffContent = (selectedConfig: object, latestConfig: object) => {
+  if (!selectedConfig || !latestConfig) {
+    return;
+  }
+  // parse the json object into string and split at newline
+  const selectedConfigArray = JSON.stringify(selectedConfig, null, 2).split('\n');
+  const latestConfigArray = JSON.stringify(latestConfig, null, 2).split('\n');
+  // the json-diff library is able to compare two arrays of strings and
+  // look for the difference of transforming left to right
+  const result = diff(selectedConfigArray, latestConfigArray);
+  if (!result) {
+    // two json are the same
+    return <h3 style={{ textAlign: 'center' }}>{T.translate(`${PREFIX}.noDiffText`)}</h3>;
+  }
+  const coloredSelectedConfig = [];
+  const coloredLatestConfig = [];
+  const selectedConfigLineNums = [];
+  const latestConfigLineNums = [];
+  let selectedConfigIdx = 0;
+  let latestConfigIdx = 0;
+  for (const array of result) {
+    // I didn't do index out of bound check here, because at the end both left and right should
+    // reach the last index of their own array
+    if (array.length === 1) {
+      // the value is [' '], means left and right are the same here
+      // so both left and right should append the current line and increment index
+      coloredSelectedConfig.push(styleSpanNormal(selectedConfigArray[selectedConfigIdx]));
+      coloredLatestConfig.push(styleSpanNormal(latestConfigArray[latestConfigIdx]));
+      selectedConfigLineNums.push(styleSpanNormal(selectedConfigIdx));
+      latestConfigLineNums.push(styleSpanNormal(latestConfigIdx));
+      selectedConfigIdx++;
+      latestConfigIdx++;
+      continue;
+    }
+    if (array[0] === '+') {
+      // value exists only in right
+      // right should append its current line with green addition style and increment index
+      // left should append an empty line
+      coloredSelectedConfig.push(<EmptyLineSpan> </EmptyLineSpan>);
+      coloredLatestConfig.push(styleSpanStringGreen(latestConfigArray[latestConfigIdx]));
+      selectedConfigLineNums.push(styleSpanNormal(' '));
+      latestConfigLineNums.push(styleSpanNormal(latestConfigIdx));
+      latestConfigIdx++;
+      continue;
+    }
+    if (array[0] === '-') {
+      // value exists only in left
+      // right should append an empty line
+      // left should append its current line with red deletion style and increment index
+      coloredSelectedConfig.push(styleSpanStringRed(selectedConfigArray[selectedConfigIdx]));
+      coloredLatestConfig.push(<EmptyLineSpan> </EmptyLineSpan>);
+      selectedConfigLineNums.push(styleSpanNormal(selectedConfigIdx));
+      latestConfigLineNums.push(styleSpanNormal(' '));
+      selectedConfigIdx++;
+    }
+  }
+  return (
+    <>
+      <div>
+        <b>{T.translate(`${PREFIX}.older`)}</b>
+        <pre>
+          <div style={{ float: 'left', width: 'auto' }}>{selectedConfigLineNums.map((x) => x)}</div>
+          <div style={{ width: 'auto' }}>{coloredSelectedConfig.map((x) => x)}</div>
+        </pre>
+      </div>
+      <div>
+        <b>{T.translate(`${PREFIX}.latest`)}</b>
+        <pre>
+          <div style={{ float: 'left', width: 'auto' }}>{latestConfigLineNums.map((x) => x)}</div>
+          <div style={{ width: 'auto' }}>{coloredLatestConfig.map((x) => x)}</div>
+        </pre>
+      </div>
+    </>
+  );
+};
+
+const styleSpanStringGreen = (val: string) => {
+  return <GreenSpan>{'+ ' + val}</GreenSpan>;
+};
+
+const styleSpanStringRed = (val: string) => {
+  return <RedSpan>{'- ' + val}</RedSpan>;
+};
+
+const styleSpanNormal = (val: string | number) => {
+  return <NormalSpan>{val}</NormalSpan>;
+};
+
 const JsonDiff = ({
   isOpen,
   onClose,
@@ -73,86 +161,6 @@ const JsonDiff = ({
   latestConfig,
   selectedConfig,
 }: IJsonDiffProps) => {
-  const JsonDiffContent = () => {
-    if (!selectedConfig || !latestConfig) {
-      return;
-    }
-    const selectedConfigArray = JSON.stringify(selectedConfig, null, 2).split('\n');
-    const latestConfigArray = JSON.stringify(latestConfig, null, 2).split('\n');
-    const result = diff(selectedConfigArray, latestConfigArray);
-    if (!result) {
-      // two json are the same
-      return <h3 style={{ textAlign: 'center' }}>{T.translate(`${PREFIX}.noDiffText`)}</h3>;
-    }
-    const coloredSelectedConfig = [];
-    const coloredLatestConfig = [];
-    const selectedConfigLineNums = [];
-    const latestConfigLineNums = [];
-    let selectedConfigIdx = 0;
-    let latestConfigIdx = 0;
-    for (const array of result) {
-      if (array.length === 1) {
-        // line is same
-        coloredSelectedConfig.push(styleSpanNormal(selectedConfigArray[selectedConfigIdx]));
-        coloredLatestConfig.push(styleSpanNormal(latestConfigArray[latestConfigIdx]));
-        selectedConfigLineNums.push(styleSpanNormal(selectedConfigIdx));
-        latestConfigLineNums.push(styleSpanNormal(latestConfigIdx));
-        selectedConfigIdx++;
-        latestConfigIdx++;
-        continue;
-      }
-      if (array[0] === '+') {
-        // left should append empty line
-        coloredSelectedConfig.push(<EmptyLineSpan> </EmptyLineSpan>);
-        coloredLatestConfig.push(styleSpanStringGreen(latestConfigArray[latestConfigIdx]));
-        selectedConfigLineNums.push(styleSpanNormal(' '));
-        latestConfigLineNums.push(styleSpanNormal(latestConfigIdx));
-        latestConfigIdx++;
-        continue;
-      }
-      if (array[0] === '-') {
-        // right should append empty line
-        coloredSelectedConfig.push(styleSpanStringRed(selectedConfigArray[selectedConfigIdx]));
-        coloredLatestConfig.push(<EmptyLineSpan> </EmptyLineSpan>);
-        selectedConfigLineNums.push(styleSpanNormal(selectedConfigIdx));
-        latestConfigLineNums.push(styleSpanNormal(' '));
-        selectedConfigIdx++;
-      }
-    }
-    return (
-      <>
-        <div>
-          <b>{T.translate(`${PREFIX}.older`)}</b>
-          <pre>
-            <div style={{ float: 'left', width: 'auto' }}>
-              {selectedConfigLineNums.map((x) => x)}
-            </div>
-            <div style={{ width: 'auto' }}>{coloredSelectedConfig.map((x) => x)}</div>
-          </pre>
-        </div>
-        <div>
-          <b>{T.translate(`${PREFIX}.latest`)}</b>
-          <pre>
-            <div style={{ float: 'left', width: 'auto' }}>{latestConfigLineNums.map((x) => x)}</div>
-            <div style={{ width: 'auto' }}>{coloredLatestConfig.map((x) => x)}</div>
-          </pre>
-        </div>
-      </>
-    );
-  };
-
-  const styleSpanStringGreen = (val: string) => {
-    return <GreenSpan>{val}</GreenSpan>;
-  };
-
-  const styleSpanStringRed = (val: string) => {
-    return <RedSpan>{val}</RedSpan>;
-  };
-
-  const styleSpanNormal = (val: string | number) => {
-    return <NormalSpan>{val}</NormalSpan>;
-  };
-
   return (
     <PipelineModeless
       title={T.translate(`${PREFIX}.viewDiffTitle`)}
@@ -164,7 +172,9 @@ const JsonDiff = ({
     >
       {loading && <LoadingSVGCentered />}
       {error && <h2>{error}</h2>}
-      {!loading && !error && <JsonDiffContainer>{JsonDiffContent()}</JsonDiffContainer>}
+      {!loading && !error && (
+        <JsonDiffContainer>{JsonDiffContent(selectedConfig, latestConfig)}</JsonDiffContainer>
+      )}
     </PipelineModeless>
   );
 };
