c8982d2e42c1ea991c7b9aac25c573775e297507
Sumeng Wang <sumengwang@google.com>
2023-04-13 14:56:41 -0700

    add featureflag eval in plugin property filter
    cast string to boolean
    
    add tests
    
    more tests

--- app/cdap/components/shared/ConfigurationGroup/utilities/DynamicPluginFilters.ts
+++ app/cdap/components/shared/ConfigurationGroup/utilities/DynamicPluginFilters.ts
@@ -99,8 +99,14 @@ export function evaluateFilter(
     return true;
   }
 
+  // convert string 'true' to boolean
+  const featureFlags = { ...window?.CDAP_CONFIG?.featureFlags };
+  for (const key in featureFlags) {
+    featureFlags[key] = featureFlags[key] === 'true';
+  }
   const typedPropertyValues = {
     ...getTypedPropertyValues(propertyValues, propertiesFromBackend),
+    featureFlags,
   };
 
   // Some upgrade scenarios leave useConnection as null,
